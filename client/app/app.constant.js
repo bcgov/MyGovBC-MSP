65f673011254964081c1eafdcad10c4eda170dd1
abbr <wenfred@gmail.com>
2016-05-23 23:30:03 -0700

    put headerFooterSvcUrl in shared.js for now.

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -6,7 +6,8 @@
 		"guest",
 		"user",
 		"admin"
-	]
+	],
+	"headerFooterSvcUrl": "https://bcgov-www2-layout-service.pathfinder.gov.bc.ca/v1/theme1/"
 })
 
 ;
--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -2,5 +2,6 @@
 
 exports = module.exports = {
   // List of user roles
-  userRoles: ['guest', 'user', 'admin']
+  userRoles: ['guest', 'user', 'admin'],
+  headerFooterSvcUrl: 'https://bcgov-www2-layout-service.pathfinder.gov.bc.ca/v1/theme1/',
 };
