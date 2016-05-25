c5b3be286d01ec1d253bdcdd5ffe66808d4e7cf0
f-w <fred.wen@gov.bc.ca>
2016-05-25 09:40:44 -0700

    send headers to browser for debugging purpose

--- server/api/profile/index.js
+++ server/api/profile/index.js
@@ -4,7 +4,10 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send({name: req.get('smgov_userdisplayname') || 'my friend'})
+  res.send({
+    name: req.get('smgov_userdisplayname') || 'my friend',
+    headers: req.headers
+  })
 })
 
 module.exports = router
