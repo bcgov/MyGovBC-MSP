ae61315e793ca2051ebd085951de4d8675a9dd5a
f-w <fred.wen@gov.bc.ca>
2016-05-26 17:38:19 -0700

    set production port to 9000 as well

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,7 +11,7 @@ module.exports = {
   // Server port
   port:   process.env.OPENSHIFT_NODEJS_PORT ||
           process.env.PORT ||
-          8080,
+          9000,
 
   // MongoDB connection options
   mongo: {
