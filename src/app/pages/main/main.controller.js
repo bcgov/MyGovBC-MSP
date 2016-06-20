78abc132edadea5b18bf56363c1329367e72782b
f-w <fred.wen@gov.bc.ca>
2016-06-20 16:35:35 -0700

    added socket.io

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -4,6 +4,11 @@ module.exports = function ($http, appConstants, $window) {
   'ngInject';
   $http.get(appConstants.profileUrl).then(response => {
     this.profile = response.data;
+    try{
+      let socket = $window.io();
+      socket.emit('message','hello');
+    }
+    catch(ex){}
   });
 }
 
