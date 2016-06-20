c32b602f5d267a8562c8fc1eb33a3fa65c08e572
f-w <fred.wen@gov.bc.ca>
2016-06-20 15:53:32 -0700

    changed controller to function format.

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -1,18 +1,9 @@
 'use strict';
-class MainController {
 
-  constructor($http, $scope, appConstants) {
-    'ngInject';
-    this.$http = $http;
-    this.appConstants = appConstants
-  }
-
-  $onInit() {
-    this.$http.get(this.appConstants.profileUrl).then(response => {
-      this.profile = response.data;
-    });
-  }
+module.exports = function ($http, appConstants, $window) {
+  'ngInject';
+  $http.get(appConstants.profileUrl).then(response => {
+    this.profile = response.data;
+  });
 }
 
-
-export default MainController;
