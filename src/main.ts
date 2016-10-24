e5df3531358633b22e90f3a36ec3c83bf3f410ef
f-w <fred.wen@gov.bc.ca>
2016-10-24 12:00:39 -0700

    removed websocket; house-keeping

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 import {enableProdMode} from '@angular/core'
 import {AppModule} from './app/app.module'
 import appConstants from './app/services/appConstants'
-import '!!file-loader?name=[name].[ext]!./favicon.ico'
+import 'file?name=[name].[ext]!./favicon.ico'
 if (appConstants.runtimeEnv === 'production') {
   enableProdMode()
 }
