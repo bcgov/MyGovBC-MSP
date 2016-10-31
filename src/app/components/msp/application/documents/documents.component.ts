1713edfb7a9a70dd70996a126aa25568ad5ee9e8
Yiling Lu <yilingl@gmail.com>
2016-10-31 02:27:41 -0700

    add complete screenflow for msp application and landing page

---
+++ src/app/components/msp/application/documents/documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './documents.component.html'
+})
+export class DocumentsComponent {
+
+}
---
+++ src/app/components/msp/application/review/review.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './review.component.html'
+})
+export class ReviewComponent {
+
+}
