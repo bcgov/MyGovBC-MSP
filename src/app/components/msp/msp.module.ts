f6891f5b44842d2f69890ed3b3fef9be93498a25
Greg Turner <greg.fun@gmail.com>
2016-11-02 11:12:45 -0700

    confirmation placeholder page

--- src/app/components/msp/msp.module.ts
+++ src/app/components/msp/msp.module.ts
@@ -1,5 +1,5 @@
-import { NgModule, Injectable } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule, Injectable} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import {MspComponent} from './msp.component';
 import {LandingComponent} from './landing/landing.component';
@@ -9,72 +9,77 @@ import {PersonalInfoComponent} from './application/personal-info/personal-info.c
 import {DocumentsComponent} from './application/documents/documents.component';
 import {AddressComponent} from './application/address/address.component';
 import {ReviewComponent} from './application/review/review.component';
+import {ConfirmationComponent} from './application/confirmation/confirmation.component';
 
 /**
  * The overall progress layout is created based on 'msp-prepare-v3-a.jpeg' in
  * https://apps.gcpe.gov.bc.ca/jira/browse/PSPDN-255?filter=16000
  */
 @NgModule({
-  imports: [
-    RouterModule.forChild([
-      {
-        path: 'msp',
-        children: [
-          {
-            path: '',
-            component: LandingComponent
-          },
+    imports: [
+        RouterModule.forChild([
+            {
+                path: 'msp',
+                children: [
+                    {
+                        path: '',
+                        component: LandingComponent
+                    },
 
-          { 
-            path: 'application', 
-            component: ApplicationComponent,
-            children: [
-              {
-                path: '',
-                redirectTo: 'prepare',
-                pathMatch: 'full' 
-              },
-              {
-                path: 'prepare',
-                component: PrepareComponent
-              },
-              {
-                path: 'personal-info',
-                component: PersonalInfoComponent
-              },
-              {
-                path: 'documents',
-                component: DocumentsComponent
-              },
-              {
-                path: 'address',
-                component: AddressComponent
-              },
-              {
-                path: 'review',
-                component: ReviewComponent
-              },
-              
-            ]
-          } 
-        ]
-      }
-    ])
-    
-  ],
-  declarations: [
-    MspComponent,
-    LandingComponent,
-    ApplicationComponent,
-    PrepareComponent,
-    PersonalInfoComponent,
-    DocumentsComponent,
-    AddressComponent,
-    ReviewComponent
-  ],
+                    {
+                        path: 'application',
+                        component: ApplicationComponent,
+                        children: [
+                            {
+                                path: '',
+                                redirectTo: 'prepare',
+                                pathMatch: 'full'
+                            },
+                            {
+                                path: 'prepare',
+                                component: PrepareComponent
+                            },
+                            {
+                                path: 'personal-info',
+                                component: PersonalInfoComponent
+                            },
+                            {
+                                path: 'documents',
+                                component: DocumentsComponent
+                            },
+                            {
+                                path: 'address',
+                                component: AddressComponent
+                            },
+                            {
+                                path: 'review',
+                                component: ReviewComponent
+                            },
+                            {
+                                path: 'confirmation',
+                                component: ConfirmationComponent
+                            },
 
-  exports: [
-  ]
+                        ]
+                    }
+                ]
+            }
+        ])
+
+    ],
+    declarations: [
+        MspComponent,
+        LandingComponent,
+        ApplicationComponent,
+        PrepareComponent,
+        PersonalInfoComponent,
+        DocumentsComponent,
+        AddressComponent,
+        ReviewComponent,
+        ConfirmationComponent
+    ],
+
+    exports: []
 })
 @Injectable()
 export class MspModule {
