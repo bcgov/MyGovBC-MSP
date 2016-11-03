85b4aa52b0b26b64e08ff9418cd3a65f032a3695
Greg Turner <greg.fun@gmail.com>
2016-11-03 13:47:15 -0700

    undoing temp build solution

--- dist/common.b04afbb5089379861454.js
+++
@@ -1,2 +0,0 @@
-!function(r){function e(t){if(o[t])return o[t].exports;var n=o[t]={exports:{},id:t,loaded:!1};return r[t].call(n.exports,n,n.exports,e),n.loaded=!0,n.exports}var o={};e.m=r,e.c=o,e.p=""}([]);
-//# sourceMappingURL=common.b04afbb5089379861454.js.map
