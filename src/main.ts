fdd2fd49004bfd1424a0a0c48a5f837ba3cd8a7b
Greg Turner <greg.fun@gmail.com>
2016-11-08 14:21:35 -0800

    disallow robots

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 import {enableProdMode} from '@angular/core'
 import {AppModule} from './app/app.module'
 import 'file?name=[name].[ext]!./favicon.ico'
+import 'file?name=[name].[ext]!./robots.txt'
 let appConstants = require('./app/services/appConstants')
 if (appConstants.default.runtimeEnv === 'production') {
   enableProdMode()
