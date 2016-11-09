e73961e2bd8c0e0ad49d558893c5a14152aae7d6
Greg Turner <greg.fun@gmail.com>
2016-11-08 16:00:03 -0800

    missing files from previous commit

---
+++ src/app/components/msp/application/personal-info/personal-details/i18n/data/en/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  firstNameLabel: 'First Name',
+  firstNameRequired: 'First name is required',
+  firstNameLength: 'First name must be longer than {{firstnameRef.errors?.minlength.requiredLength-1}} characters.',
+  removeSpouseButton: 'Remove',
+  removeChildButton: 'Remove',
+  namesHelpBlock: 'Please enter legal names as they appear on the ID you will be providing.'
+}
