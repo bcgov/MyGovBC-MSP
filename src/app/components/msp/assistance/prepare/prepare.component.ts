be46ee65416f0426be7f29d9c06be082326ce3e8
Yiling Lu <yilingl@gmail.com>
2016-11-09 14:01:00 -0800

    file loader impl in progress

--- src/app/components/msp/assistance/prepare/prepare.component.ts
+++ src/app/components/msp/assistance/prepare/prepare.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+require('./prepare.component.less');
 
 @Component({
   templateUrl: './prepare.component.html'
--- src/app/components/msp/common/file-uploader/file-uploader.component.ts
+++ src/app/components/msp/common/file-uploader/file-uploader.component.ts
@@ -1,5 +1,5 @@
-import {Component, ViewChild, ElementRef} from '@angular/core';
-import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 require('./file-uploader.component.less')
 
@@ -10,64 +10,91 @@ require('./file-uploader.component.less')
 export class FileUploaderComponent {
   @ViewChild('imgRef') imageElement: ElementRef;
 
-  private trustedUrl:SafeUrl;
-  private reader: FileReader;
+  private trustedUrl: SafeUrl;
   private maxFileSize: number;
+  private fileSize: string;
+  private fileSizeUnit: string;
+  private fileName: string;
+  private fileSizeError: string;
+  private imageFileContent:string;
 
   constructor(private sanitizer: DomSanitizer) {
-    this.reader  = new FileReader();
-    this.maxFileSize = 5*1024*1024;
+    this.maxFileSize = 5 * 1024 * 1024;
   }
-  onChange(evt: any){
+  onChange(evt: any) {
+    this.trustedUrl = null;
+
     console.log(evt);
     let fileList = evt.srcElement.files;
     // console.log('file list size: ' + fileList.length);
     let file = fileList[0];
     let nBytes = file.size;
-
-    if(nBytes > this.maxFileSize){
-      console.error('This file was not accepted because its size exceeded max allowed file size (5MB).');
-    }
+    this.fileName = file.name.substring(0,60);
 
     var sOutput = nBytes + " bytes";
     // optional code for multiples approximation
     for (var aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"], nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
       sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+      this.fileSize = nApprox.toFixed(0);
+      this.fileSizeUnit = aMultiples[nMultiple];
     }
     console.log('size of selected file: ' + sOutput);
+    if (nBytes > this.maxFileSize) {
+      this.fileSizeError = 'This file was not accepted because its size exceeded max allowed file size (5MB).';
+    } else {
 
-    // let blob = this.reader.readAsDataURL(file);
-    // console.log('file blob:');
-    // console.log(blob);
+      let reader = new FileReader();
 
-    var blob_url = window.URL.createObjectURL(file);    
+      let imageEl = this.imageElement;
 
-    let image = new Image();
-    image.addEventListener('load', function(loadEvent){
-      console.log('innner load event:');
-      console.log(loadEvent);
-      console.log(loadEvent.target);
-    });
+      reader.onload = function(e){
+        let imageContent = reader.result;
+        let imageObject = new Image();
+        imageObject.src = imageContent;
 
-    image.src = file;
-    // image.src = blob_url;
-    // console.log('width: ' + image.naturalWidth + ' and height: ' + image.naturalHeight);
+        imageEl.nativeElement.src = imageContent;
+        let h = imageObject.naturalHeight;
+        let w = imageObject.naturalWidth;
 
-    this.trustedUrl = this.sanitizer.bypassSecurityTrustUrl(blob_url);;
-    console.log('file blog url: ' + blob_url);
+        console.log('reading image height and width: ' + h + 'x' + w);
+      };
+
+      reader.readAsDataURL(file);
+
+      var blob_url = window.URL.createObjectURL(file);
+      // let blob = this.reader.readAsDataURL(file);
+      // console.log('file blob:');
+      // console.log(blob);
+
+
+      // let image = new Image();
+      // image.addEventListener('load', function (loadEvent) {
+      //   console.log('innner load event:');
+      //   console.log(loadEvent);
+      //   console.log(loadEvent.target);
+      // });
+
+      // image.src = file;
+      // image.src = blob_url;
+      // console.log('width: ' + image.naturalWidth + ' and height: ' + image.naturalHeight);
+
+      this.trustedUrl = this.sanitizer.bypassSecurityTrustUrl(blob_url);;
+      console.log('file blog url: ' + blob_url);
+    }
+
   }
 
-  onLoad(event:Event){
+  onLoad(event: Event) {
     console.log('img onload event');
     console.log(this.imageElement.nativeElement);
-    var target = event.target ? event.target : event.srcElement;    
+    var target = event.target ? event.target : event.srcElement;
     // let image = evt.target;
     // img.src = 
-    console.log('width: ' + this.imageElement.nativeElement.naturalWidth + ' and height: ' 
+    console.log('width: ' + this.imageElement.nativeElement.naturalWidth + ' and height: '
       + this.imageElement.nativeElement.naturalHeight);
   }
 
-  get imageUrl(){
+  get imageUrl() {
     return this.trustedUrl;
   }
 }
