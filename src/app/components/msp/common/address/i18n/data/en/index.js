bb862760afe7d62567b7f532056e1ab04a105797
Greg Turner <greg.fun@gmail.com>
2016-11-17 10:37:52 -0800

    mailing address has province select and personal info done

--- src/app/components/msp/common/address/i18n/data/en/index.js
+++ src/app/components/msp/common/address/i18n/data/en/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   streetLabel: 'Street',
   cityLabel: 'City',
+  provinceLabel: 'Province',
   postalCodeLabel: 'Postal Code',
   mailingAddressSameQuestion: 'Is your <strong>mailing address</strong> the same as above?',
   mailingAddressHeading: 'Mailing Address',
