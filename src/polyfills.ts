9747d0a31ed22afd8947070bd443819e5c0d418c
Greg Turner <greg.fun@gmail.com>
2016-11-17 21:21:17 -0800

    ie9 shim installed, works better in IE9 but still odd

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,10 +1,13 @@
-import 'core-js/es6'
-import 'core-js/es7/reflect'
-require('zone.js/dist/zone')
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+require('zone.js/dist/zone');
+
 if (process.env.ENV === 'production') {
   // Production
+  require('angular2-ie9-shims/shims_for_IE.prod.js');
 } else {
   // Development
-  Error['stackTraceLimit'] = Infinity
-  require('zone.js/dist/long-stack-trace-zone')
+  Error['stackTraceLimit'] = Infinity;
+  require('zone.js/dist/long-stack-trace-zone');
+  require('angular2-ie9-shims/shims_for_IE.dev.js');
 }
