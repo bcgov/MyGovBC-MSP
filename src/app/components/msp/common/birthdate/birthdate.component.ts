7840f33f0e5526bf0ee878c7e5ea871343ad90a7
Greg Turner <greg.fun@gmail.com>
2016-12-01 15:39:23 -0800

    dob wired in but still missing some business rules

--- src/app/components/msp/common/birthdate/birthdate.component.ts
+++ src/app/components/msp/common/birthdate/birthdate.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject} from '@angular/core'
+import {Component, Input} from '@angular/core'
+import {Person} from "../../model/person.model";
+require('./birthdate.component.less');
 
-require('./birthdate.component.less')
 @Component({
   selector: 'msp-birthdate',
   templateUrl: './birthdate.component.html'
@@ -9,6 +10,6 @@ export class MspBirthDateComponent {
 
     lang = require('./i18n');
 
-    constructor(@Inject('appConstants') appConstants: Object) {
-    }
+    @Input() person: Person;
+
 }
--- src/app/components/msp/common/birthdate/i18n/data/en/index.js
+++ src/app/components/msp/common/birthdate/i18n/data/en/index.js
@@ -1,3 +1,6 @@
 module.exports = {
   birthdateLabel: 'Birthdate',
+  monthErrorRequired: 'Month is required',
+  dayErrorRequired: 'Day is required',
+  yearErrorRequired: 'Year is required'
 }
