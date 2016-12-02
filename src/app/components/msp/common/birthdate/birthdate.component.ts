ad85673d49b69aa045aa37158c37c96b7f8fe190
Greg Turner <greg.fun@gmail.com>
2016-12-02 10:10:34 -0800

    moment added for birthdate control with business rules

--- src/app/components/msp/common/birthdate/birthdate.component.ts
+++ src/app/components/msp/common/birthdate/birthdate.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input} from '@angular/core'
 import {Person} from "../../model/person.model";
+import {Relationship, Activities} from "../../model/status-activities-documents";
+import * as moment from 'moment';
+
 require('./birthdate.component.less');
 
 @Component({
@@ -8,8 +11,68 @@ require('./birthdate.component.less');
 })
 export class MspBirthDateComponent {
 
-    lang = require('./i18n');
+  lang = require('./i18n');
 
-    @Input() person: Person;
+  // Create today for comparison in check later
+  today = moment();
+
+  // Parse person's date
+  dob() {
+    return moment({
+      year: this.person.dob_year,
+      month: this.person.dob_month - 1, // moment use 0 index for month :(
+      day: this.person.dob_day,
+    });
+  }
+
+
+  @Input() person: Person;
+
+  /**
+   * Determine if date of birth is valid for the given person
+   *
+   * @returns {boolean}
+   */
+  isValid(): boolean {
+
+    // Validate
+    if (!this.dob().isValid()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  futureCheck(): boolean {
+
+    // Check not in future
+    if (this.dob().isAfter(this.today)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  ageCheck(): boolean {
+
+    // Child rules
+    if (this.person.relationship === Relationship.Child) {
+
+      // if child student must be between 19 and 24
+      if (this.person.currentActivity === Activities.StudyingInBC) {
+        if (!this.dob().isBetween(this.today.subtract(19, 'years'), this.today.subtract(24,'years'))) {
+          return false;
+        }
+      }
+      else {
+        // must be less than 19 if not in school
+        if (!this.dob().isAfter(this.today.subtract(19, 'years'))) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 
 }
--- src/app/components/msp/common/birthdate/i18n/data/en/index.js
+++ src/app/components/msp/common/birthdate/i18n/data/en/index.js
@@ -2,5 +2,9 @@ module.exports = {
   birthdateLabel: 'Birthdate',
   monthErrorRequired: 'Month is required',
   dayErrorRequired: 'Day is required',
-  yearErrorRequired: 'Year is required'
+  yearErrorRequired: 'Year is required',
+  yearErrorBadFormat: 'This does not appear to be a valid date',
+  yearErrorFutureCheck: 'Birth date can not be in the future',
+  yearErrorAgeCheck: 'A child must be less than 19 years old and a post-secondary student must be between 19 and 24 years',
+
 }
