32df11c54a23a3110b02a206dcf4381ec304f5ab
Greg Turner <greg.fun@gmail.com>
2016-12-09 12:54:30 -0800

    assistance confirmation up-to-date and aligned with apply

--- src/app/components/msp/assistance/confirmation/i18n/data/en/index.js
+++ src/app/components/msp/assistance/confirmation/i18n/data/en/index.js
@@ -1,10 +1,9 @@
 module.exports = {
   pageTitle: '<i class="fa fa-check success" aria-hidden="true"></i> You have successfully submitted your application for finacial assistance',
-  secondTitle: 'What happens next...',
-  printEmailInstructions: '<a href="#" target="_blank">Print your application</a> for your records, or email yourself a copy',
+  secondTitle: 'What happens next',
+  printEmailInstructions: '<a>Print your application</a> for your records',
   emailAddressLabel: 'Email address',
   sendEmailButton: 'Send a copy',
   generalInstructions1: 'If your application is approved, you will see an adjustment on your next invoice.',
   generalInstructions2: 'If your application is not approved, you will receive a letter of explanation at the address you provided',
-  generalInstructions3: 'File your taxes by April 30 every year to maintain your eligibility for financial assistance.'
 }
