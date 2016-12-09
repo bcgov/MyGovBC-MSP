dcdebe9bed3f89edf3f302933b02ff3453c54652
Greg Turner <greg.fun@gmail.com>
2016-12-09 13:21:09 -0800

    configured links

--- src/app/components/msp/application/confirmation/i18n/data/en/index.js
+++ src/app/components/msp/application/confirmation/i18n/data/en/index.js
@@ -3,8 +3,8 @@ module.exports = {
   secondTitle: 'What happens next',
   nextStep1: "<a>Print your application</a> for your records",
   nextStep2: "Your coverage should start in approximately one month, based on your arrival date in " +
-  "B.C.. Learn more about the <a>wait period</a>.",
+  "B.C.. Learn more about the <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/eligibility-and-enrolment/how-to-enrol/coverage-wait-period' target='_blank'>wait period</a> <i class='fa fa-external-link' aria-hidden='true'></i>.",
   nextStep3: "Before your coverage starts, you will receive a bill at the mailing address you provided.",
-  nextStep4: "You may be eligible for <a>Premium Assistance</a> (financial help with paying your " +
+  nextStep4: "You may be eligible for <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/premiums/regular-premium-assistance' target='_blank'>Premium Assistance</a> <i class='fa fa-external-link' aria-hidden='true'></i> (financial help with paying your " +
   "premiums) if your approximate combined family annual income is less than $40,000.",
 }
