757e1e31d051ccc07faeb5be05351ce9f2f973a5
megwhetung <meg.whetung@gmail.com>
2016-12-09 14:26:01 -0800

    Edited to match Sarah's latest draft

--- src/app/components/msp/application/confirmation/i18n/data/en/index.js
+++ src/app/components/msp/application/confirmation/i18n/data/en/index.js
@@ -1,10 +1,9 @@
 module.exports = {
-  pageTitle: '<i class="fa fa-check success" aria-hidden="true"></i> You have successfully submitted your application for health care',
+  pageTitle: '<i class="fa fa-check success" aria-hidden="true"></i> Your application has been submitted.',
   secondTitle: 'What happens next',
   nextStep1: "<a>Print your application</a> for your records",
-  nextStep2: "Your coverage should start in approximately one month, based on your arrival date in " +
+  nextStep2: "<b>If you qualify</b>, your coverage should start in about one month from the time you apply, depending on when you arrived in " +
   "B.C.. Learn more about the <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/eligibility-and-enrolment/how-to-enrol/coverage-wait-period' target='_blank'>wait period</a> <i class='fa fa-external-link' aria-hidden='true'></i>.",
-  nextStep3: "Before your coverage starts, you will receive a bill at the mailing address you provided.",
-  nextStep4: "You may be eligible for <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/premiums/regular-premium-assistance' target='_blank'>Premium Assistance</a> <i class='fa fa-external-link' aria-hidden='true'></i> (financial help with paying your " +
-  "premiums) if your approximate combined family annual income is less than $40,000.",
+  nextStep3: "Before your coverage starts, you will be mailed a bill. Once you receive your bill, visit ICBC to get your BC Services Card.",
+  nextStep4: "Low income families who have lived in B.C. for at least a year may also be eligible for financial help to paying the monthly premium. Find out about <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/premiums/regular-premium-assistance' target='_blank'>Premium Assistance</a> <i class='fa fa-external-link' aria-hidden='true'></i>",
 }
