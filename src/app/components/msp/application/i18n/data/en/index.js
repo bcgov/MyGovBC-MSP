9e5a0d8b7bb087697faa78a83112415b413e255a
megwhetung <meg.whetung@gmail.com>
2016-12-09 16:06:23 -0800

    update tab labels to match latest design

--- src/app/components/msp/application/i18n/data/en/index.js
+++ src/app/components/msp/application/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   serviceName: 'Apply for MSP',
-  progressStep1: 'Prepare',
-  progressStep2: 'Personal Info',
+  progressStep1: 'Check Eligibility',
+  progressStep2: 'Personal Info & Documents',
   progressStep3: 'Contact Info',
   progressStep4: 'Review & Submit'
 }
