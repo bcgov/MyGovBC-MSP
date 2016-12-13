341bf62277ca3578f88eff6c03609fe5325799f8
megwhetung <meg.whetung@gmail.com>
2016-12-13 12:25:01 -0800

    change "alternative" to "alternate"

--- src/app/components/msp/common/phone/i18n/data/en/index.js
+++ src/app/components/msp/common/phone/i18n/data/en/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   phoneLabel: 'Phone number',
-  altPhoneLabel: 'Alternative phone number',
+  altPhoneLabel: 'Alternate phone number',
 
   phoneErrorRequired: 'Phone number is required',
   phoneErrorPattern: 'Must be in the format 555-123-4567 or 5551234567'
