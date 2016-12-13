6965aa63e7669a316fc6f8739bd6737ae528005b
Greg Turner <greg.fun@gmail.com>
2016-12-13 15:49:54 -0800

    a sin validator, not wired in yet

---
+++ src/app/components/msp/common/sin/sin.validator.spec.ts
@@ -0,0 +1,26 @@
+import {TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {SinCheckValidator} from './sin.validator';
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
+
+describe('SIN Component', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SinCheckValidator],
+      imports: [BrowserModule,
+        CommonModule,
+        FormsModule]
+    })
+  });
+  it('should validate SIN properly', () => {
+    let fixture = new SinCheckValidator();
+
+    expect(fixture.isValid("")).toBe(false, 'empty value should not pass');
+    expect(fixture.isValid(null)).toBe(false, 'null should not pass');
+    expect(fixture.isValid("A")).toBe(false, 'a letter should not pass');
+    expect(fixture.isValid(" A ")).toBe(false, 'a letter should not pass');
+    expect(fixture.isValid("123456789")).toBe(false, 'not a good sin number');
+    expect(fixture.isValid("046454286")).toBe(true, 'a good sin number');
+  });
+})
---
+++ src/app/components/msp/common/sin/sin.validator.ts
@@ -0,0 +1,83 @@
+import {Directive, forwardRef, Input} from '@angular/core';
+import {Validator, NG_VALIDATORS, FormControl} from '@angular/forms';
+
+@Directive({
+  selector: '[sinCheck][ngModel]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: forwardRef(() => SinCheckValidator), multi: true
+    }
+  ],
+  host: {'[attr.mod11Check]': 'mod11Check ? mod11Check : null'}
+})
+
+export class SinCheckValidator implements Validator {
+
+  validate(control: FormControl): {[key:string]:boolean;}  {
+
+    // Get value out of control
+    let sin = control.value;
+
+    // pre req checks
+    if (sin == null ||
+      sin.length < 1) return null;
+
+    if (this.isValid(sin)) {
+      // return null for no errors
+      return null;
+    }
+    else {
+      // return "true" if we have errors
+      return {"sinCheck": true}
+    }
+  }
+
+  isValid (sin: string): boolean {
+
+    // pre req checks
+    if (sin == null ||
+      sin.length < 1) return false;
+
+    // Init weights and other stuff
+    let weights:number[] = [1,2,1,2,1,2,1,2,1];
+    let sum = 0;
+
+    // Clean up string
+    sin = sin.trim();
+
+    // Rip off spaces
+    sin = sin.replace(" ", "");
+
+    // Test for length
+    if (sin.length != 9) return false;
+
+    // Walk through each character
+    for (let i = 0; i < sin.length; i++) {
+
+      // pull out char
+      let char = sin.charAt(i);
+
+      // parse the number
+      let num = Number(char);
+      if (Number.isNaN(num)) return false;
+
+      // multiply the value against the weight
+      let result = num * weights[i];
+
+      // If two digit result, substract 9
+      if (result > 9) {
+        result = result - 9;
+      }
+
+      // add it to our sum
+      sum += result;
+    }
+
+    // The sum must be divisble by 10
+    if (sum % 10 != 0) {
+      return false;
+    }
+
+    // All done!
+    return true;
+  }
+}
