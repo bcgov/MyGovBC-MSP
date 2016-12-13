40547097e07b225dfe79798240414702592bf29d
megwhetung <meg.whetung@gmail.com>
2016-12-13 12:10:08 -0800

    edit headings for children info and disability info

--- src/app/components/msp/assistance/prepare/i18n/data/en/index.js
+++ src/app/components/msp/assistance/prepare/i18n/data/en/index.js
@@ -5,8 +5,8 @@ module.exports = {
   ageGroupRequired: 'Please specify age group',
   spouseCommonLawStatusRequired: 'Please specify spouse/common-law status',
   continueButton: 'Continue to apply for financial assistance',
-  disabilityInfo: 'Disability Information',
-  childrenInfo: 'Children Information',
+  disabilityInfo: 'Add disability information',
+  childrenInfo: 'Add children information',
   invalidNumber: 'Must use a valid number'
 }
 
