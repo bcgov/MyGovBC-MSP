26a14c40c6b2d882bb48cfbf98218851d4e6fc4c
megwhetung <meg.whetung@gmail.com>
2016-12-14 12:43:16 -0800

    Edited to match Sarah's latest draft

--- src/app/components/msp/assistance/authorize-submit/i18n/data/en/index.js
+++ src/app/components/msp/assistance/authorize-submit/i18n/data/en/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  pageTitle: 'Can we check your income tax return information?',
-  helpBlock: 'In order to process your application, we need to contact Canada Revenue Agency to verify the information you have provided. If you file your taxes by April 30 each year, you will not need to reapply for financial assistance.',
+  pageTitle: 'Give permission for your income tax information to be checked',
+  helpBlock: 'Each applicant must agree to have their income tax information confirmed with Canada Revenue Agency.',
   declarationAndConsentTitle: 'Declaration and Consent',
   doYouAgreeLabel: '{name}, do you agree?',
   agreeLabel: 'Yes, I agree',
