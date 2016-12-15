bc7c55cb9128cc3b6a80951f2256326700398fe6
megwhetung <meg.whetung@gmail.com>
2016-12-15 11:40:35 -0800

    fixed punctuation

--- src/app/components/msp/assistance/personal-info/i18n/data/en/index.js
+++ src/app/components/msp/assistance/personal-info/i18n/data/en/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   pageTitle: 'Tell us a bit about who’s applying for assistance',
-  helpBlock: 'Enter each person\'s legal name as it appears on their official Canadian identity documents, .e.g, birth' +
+  helpBlock: 'Enter each person\'s legal name as it appears on their official Canadian identity documents, e.g., birth' +
   ' certificate, permanent resident card, passport.',
 
   applicantInfo: 'Your information',
