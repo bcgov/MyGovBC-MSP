80d38b8e330f8e395cbeecf4c4627d2877d1e594
megwhetung <meg.whetung@gmail.com>
2016-12-15 11:38:27 -0800

    add links re Service BC and services card

--- src/app/components/msp/application/confirmation/i18n/data/en/index.js
+++ src/app/components/msp/application/confirmation/i18n/data/en/index.js
@@ -4,6 +4,6 @@ module.exports = {
   nextStep1: "<a>Print your application</a> for your records",
   nextStep2: "<b>If you qualify</b>, your coverage should start in about one month from the time you apply, depending on when you arrived in " +
   "B.C.. Learn more about the <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/eligibility-and-enrolment/how-to-enrol/coverage-wait-period' target='_blank'>wait period</a> <i class='fa fa-external-link' aria-hidden='true'></i>.",
-  nextStep3: "Before your coverage starts, you will be mailed a bill. Once you receive your bill, visit ICBC to get your BC Services Card.",
+  nextStep3: "Before your coverage starts, you will be mailed a bill. Once you receive your bill, visit ICBC or <a href='http://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/technology-innovation-and-citizens-services/servicebc' target='blank'>Service BC</a> to get your <a href='http://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card' target='blank'>BC Services Card</a>.",
   nextStep4: "Low income families who have lived in B.C. for at least a year may also be eligible for financial help to paying the monthly premium. Find out about <a href='http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/premiums/regular-premium-assistance' target='_blank'>Premium Assistance</a> <i class='fa fa-external-link' aria-hidden='true'></i>",
 }
