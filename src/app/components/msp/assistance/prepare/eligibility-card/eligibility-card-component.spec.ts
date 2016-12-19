b3c05887dded2aac9f66707960726897adde1124
Greg Turner <greg.fun@gmail.com>
2016-12-19 12:17:52 -0800

    more unit tests

---
+++ src/app/components/msp/assistance/prepare/eligibility-card/eligibility-card-component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing'
+import { FormsModule} from '@angular/forms';
+
+import {EligibilityCardComponent} from './eligibility-card.component';
+import MspDataService from '../../../service/msp-data.service';
+import { LocalStorageService, LOCAL_STORAGE_SERVICE_CONFIG } from 'angular-2-local-storage';
+import {RouterTestingModule} from "@angular/router/testing";
+
+describe('DeductionCalculatorComponent', () => {
+  let localStorageServiceConfig = {
+    prefix: 'ca.bc.gov.msp',
+    storageType: 'localStorage'
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EligibilityCardComponent],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [MspDataService,
+        LocalStorageService,{
+          provide: LOCAL_STORAGE_SERVICE_CONFIG, useValue: localStorageServiceConfig
+        }
+      ]
+    })
+  });
+  it ('should work', () => {
+    let fixture = TestBed.createComponent(EligibilityCardComponent);
+    expect(fixture.componentInstance instanceof EligibilityCardComponent).toBe(true, 'should create EligibilityCardComponent');
+  });
+})
