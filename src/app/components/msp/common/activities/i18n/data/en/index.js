45e2bf8ea930248414cc38d9e927b4b4cfed613a
megwhetung <meg.whetung@gmail.com>
2016-12-21 14:40:54 -0800

    correcting spouse option language

--- src/app/components/msp/common/activities/i18n/data/en/index.js
+++ src/app/components/msp/common/activities/i18n/data/en/index.js
@@ -4,8 +4,8 @@ module.exports = {
   2: 'Moving from another country',
   3: 'Working in BC',
   4: 'Studying in BC',
-  5: 'Religious work',
-  6: 'Spouse or dependants',
+  5: 'Religious worker',
+  6: 'Spouse on a visitor visa',
   7: 'Diplomat'
 }
 /**
