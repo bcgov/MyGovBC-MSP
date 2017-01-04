f182bb037d49003a42cc8fefa10d849b1145b15a
megwhetung <meg.whetung@gmail.com>
2017-01-04 09:21:19 -0800

    Clarify student status of older children
    Re: MoH legal feedback

--- src/app/components/msp/application/personal-info/i18n/data/en/index.js
+++ src/app/components/msp/application/personal-info/i18n/data/en/index.js
@@ -2,6 +2,6 @@ module.exports = {
   pageTitle: 'Tell us a bit about who is applying for health care coverage',
   addSpouseButton: 'Add Spouse/Common-Law Partner',
   addChildUnder19Button: 'Add Child (0-18)',
-  addChild19To24Button: 'Add Child (19-24)',
+  addChild19To24Button: 'Add Child (19-24) who is a full-time student',
   continueButton: 'Continue'
 }
