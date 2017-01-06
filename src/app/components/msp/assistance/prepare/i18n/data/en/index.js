a082a8a055fe373fe160aa13c2c25d199bfde172
megwhetung <meg.whetung@gmail.com>
2017-01-06 09:35:37 -0800

    change Financial Assistance to Premium Assistance

--- src/app/components/msp/assistance/prepare/i18n/data/en/index.js
+++ src/app/components/msp/assistance/prepare/i18n/data/en/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  checkEligibilityScreenTitle: 'See if you qualify for financial assistance to pay for public health care coverage',
+  checkEligibilityScreenTitle: 'See if you qualify for Premium Assistance to pay for public health care coverage',
   incomeDataRequired: 'Please provide income information',
   incomeDataMaxLength: 'Too many characters',
   ageGroupRequired: 'Please specify age group',
   spouseCommonLawStatusRequired: 'Please specify spouse/common-law status',
-  continueButton: 'Continue to apply for financial assistance',
+  continueButton: 'Continue to apply for Premium Assistance',
   disabilityInfo: 'Add disability information',
   childrenInfo: 'Add children information',
   invalidNumber: 'Must use a valid number'
