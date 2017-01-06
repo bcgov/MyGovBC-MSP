a032fcdffb46dd612d3f98baa2dc1f1de2542592
f-w <fred.wen@gov.bc.ca>
2017-01-06 11:38:37 -0800

    make unit test less stringent.

--- src/app/components/msp/assistance/prepare/prepare.component.spec.ts
+++ src/app/components/msp/assistance/prepare/prepare.component.spec.ts
@@ -1,35 +1,35 @@
-import { TestBed } from '@angular/core/testing'
-import { Component, ViewChild, AfterViewInit, OnInit, ElementRef} from '@angular/core';
-import { FormsModule, FormGroup, NgForm, AbstractControl } from '@angular/forms';
+import {TestBed} from '@angular/core/testing'
+import {Component, ViewChild, AfterViewInit, OnInit, ElementRef} from '@angular/core';
+import {FormsModule, FormGroup, NgForm, AbstractControl} from '@angular/forms';
 
-import { AssistancePrepareComponent } from './prepare.component'
+import {AssistancePrepareComponent} from './prepare.component'
 import {DeductionCalculatorComponent} from './deduction-calculator/deduction-calculator.component';
 import MspDataService from '../../service/msp-data.service';
-import { LocalStorageService, LOCAL_STORAGE_SERVICE_CONFIG } from 'angular-2-local-storage';
+import {LocalStorageService, LOCAL_STORAGE_SERVICE_CONFIG} from 'angular-2-local-storage';
 
 describe('AssistancePrepareComponent', () => {
   let localStorageServiceConfig = {
-      prefix: 'ca.bc.gov.msp',
-      storageType: 'localStorage'
+    prefix: 'ca.bc.gov.msp',
+    storageType: 'localStorage'
   };
-  
+
   beforeEach(() => {
-    TestBed.configureTestingModule({ 
+    TestBed.configureTestingModule({
       declarations: [AssistancePrepareComponent,
-      DeductionCalculatorComponent],
+        DeductionCalculatorComponent],
       imports: [FormsModule],
       providers: [MspDataService,
-        LocalStorageService,{
-            provide: LOCAL_STORAGE_SERVICE_CONFIG, useValue: localStorageServiceConfig
-        }    
+        LocalStorageService, {
+          provide: LOCAL_STORAGE_SERVICE_CONFIG, useValue: localStorageServiceConfig
+        }
       ]
-  })
+    })
   });
-  it ('should work', () => {
+  it('should work', () => {
     let fixture = TestBed.createComponent(AssistancePrepareComponent)
     expect(fixture.componentInstance instanceof AssistancePrepareComponent).toBe(true, 'should create AssistancePrepareComponent')
     expect(fixture.componentInstance.lang('./en/index').checkEligibilityScreenTitle)
-      .toBe('See if you qualify for financial assistance to pay for public health care coverage',
-      'Should match what is i18n file.');
+      .toContain('See if you qualify',
+        'Should match what is i18n file.')
   });
 })
