e5a42234871a3257f63ba22db01c96830735a309
Greg Turner <greg.fun@gmail.com>
2017-01-06 09:35:47 -0800

    added msp-api-service stub

---
+++ src/app/components/msp/service/msp-api.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {MspApplication} from "../model/application.model";
+
+@Injectable()
+export class MspApiService {
+  convertViewModel(application: MspApplication):
+}
