203fee3106402bcfce78ab2bdd3d5668511f8ffe
megwhetung <meg.whetung@gmail.com>
2017-01-16 09:42:46 -0800

    New language from Sarah for bad answers

--- src/app/components/msp/application/prepare/i18n/data/en/index.js
+++ src/app/components/msp/application/prepare/i18n/data/en/index.js
@@ -2,13 +2,13 @@ module.exports = {
   pageTitle: 'See if you qualify for public health care in B.C.',
 
   question1: 'Do you currently live in British Columbia (i.e. Do you have an address here)?',
-  badAnswer1: 'You must live in British Columbia in order to apply for public health insurance here.',
+  badAnswer1: 'You might not qualify for public health insurance if you do not live in B.C. Contact <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a> for more information.',
 
   question2: 'Are you planning to stay for six months or longer?',
-  badAnswer2: 'You must live in B.C. for at least six months in order to qualify for public health care insurance.',
+  badAnswer2: 'You might not qualify for public health insurance if you do not live in B.C. for at least six months. Contact <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a> for more information.',
 
   question3: 'Are you planning to be away from B.C. for more than 30 days in total in the next six months?',
-  badAnswer3: 'If you leave B.C. for more than 30 days in total during the first six months after applying, you’re no longer considered a B.C. resident and will not qualify for public health care. If you’re planning to take a trip that’s longer than 30 days, apply for MSP when you return.',
+  badAnswer3: 'You might not qualify for public health insurance if you leave the province for more than 30 days during the first six months after you apply - doing this could mean you are no longer considered a B.C. resident. Find out more by contacting <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a>.',
 
   question4: 'Is anyone you\'re applying for',
   question4sub: '<ul><li>An active member of the Canadian Forces</li>' +
