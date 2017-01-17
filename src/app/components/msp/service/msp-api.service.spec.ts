d84833132a5377aa02fe2d2b3e1c1adec33121ec
Greg Turner <greg.fun@gmail.com>
2017-01-16 22:12:11 -0800

    fixed test/build issues

--- src/app/components/msp/service/msp-api.service.spec.ts
+++ src/app/components/msp/service/msp-api.service.spec.ts
@@ -8,7 +8,28 @@ import {HttpModule} from "@angular/http";
 import appConstants from '../../../services/appConstants';
 import {Data} from "./test/image.data";
 import {FinancialAssistApplication} from "../model/financial-assist-application.model";
-let base64ToBlob = require("base64ToBlob");
+
+function base64ToBlob(base64:any, mime:any) {
+  mime = mime || '';
+  var sliceSize = 1024;
+  var byteChars = window.atob(base64);
+  var byteArrays = <any>[];
+
+  for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
+    var slice = byteChars.slice(offset, offset + sliceSize);
+
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+
+  return new Blob(byteArrays, {type: mime});
+}
 
 describe('MspApiService', () => {
 
