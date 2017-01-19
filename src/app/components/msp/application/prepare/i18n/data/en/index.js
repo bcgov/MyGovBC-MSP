5154913412df01c268e215ad32c67a52126b10ef
megwhetung <meg.whetung@gmail.com>
2017-01-19 15:46:01 -0800

    clarify consecutive vs. total in question 3 answer
    Re: MoH feedback

--- src/app/components/msp/application/prepare/i18n/data/en/index.js
+++ src/app/components/msp/application/prepare/i18n/data/en/index.js
@@ -8,7 +8,7 @@ module.exports = {
   badAnswer2: 'If you are planning on returning to a residence in another province or country, you might not qualify for public health insurance in BC. Please contact <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a> for assistance with your application.',
 
   question3: 'Are you planning to be away from B.C. for more than 30 days in total in the next six months?',
-  badAnswer3: 'You might not qualify for public health insurance if you leave the province for more than 30 days during the first six months after you apply - doing this could mean you are no longer considered a B.C. resident. Find out more by contacting <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a>.',
+  badAnswer3: 'You might not qualify for public health insurance if you leave the province for more than 30 days in total during the first six months after you apply - doing this could mean you are no longer considered a B.C. resident. Find out more by contacting <a href="http://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us" target="_blank">Health Insurance BC</a>.',
 
   question4: 'Is anyone you\'re applying for',
   question4sub: '<ul><li>An active member of the Canadian Forces</li>' +
