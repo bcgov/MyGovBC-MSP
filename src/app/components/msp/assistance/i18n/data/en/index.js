64b5fe6a96cdab5a07e9e5048d289c9b3a008d8f
megwhetung <meg.whetung@gmail.com>
2017-01-27 13:14:03 -0800

    change new step label to "Select Years"

--- src/app/components/msp/assistance/i18n/data/en/index.js
+++ src/app/components/msp/assistance/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   serviceName: 'Apply for Premium Assistance',
   progressStep1: 'Check Eligibility',
-  progressStep2: 'Application for this and past years',
+  progressStep2: 'Select years',
   progressStep3: 'Personal & Contact Info',
   progressStep4: 'Review',
   progressStep5: 'Authorize & Submit'
