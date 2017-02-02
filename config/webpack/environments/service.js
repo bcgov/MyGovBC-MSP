2bce830905c2094a2685043f79bffe6d9e7b4ea8
f-w <fred.wen@gov.bc.ca>
2017-02-01 17:44:29 -0800

    added logging service

--- config/webpack/environments/service.js
+++ config/webpack/environments/service.js
@@ -2,7 +2,8 @@
 module.exports = function(_path) {
   return {
     appConstants: {
-      serviceName: 'Apply for BC Health Care'
+      serviceName: 'Apply for BC Health Care',
+      logBaseUrl: 'http://logstash-gcpe-mygovbc-msp-dev.pathfinder.gov.bc.ca',
     }
   }
 }
