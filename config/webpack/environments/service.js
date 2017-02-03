8899ce73ab47561dbe5da08ce576ebf513a22b65
f-w <fred.wen@gov.bc.ca>
2017-02-03 15:06:37 -0800

    updated config instruction

--- config/webpack/environments/service.js
+++ config/webpack/environments/service.js
@@ -4,6 +4,8 @@ module.exports = function(_path) {
     appConstants: {
       serviceName: 'Apply for BC Health Care',
       logBaseUrl: '/api/logging',
+      // to test logging api in your localhost dev env, don't change above config. Instead,
+      // put following config entry in file ./local.js, which is ignored by git
       // logBaseUrl: 'http://logstash-gcpe-mygovbc-msp-dev.pathfinder.gov.bc.ca',
     }
   }
