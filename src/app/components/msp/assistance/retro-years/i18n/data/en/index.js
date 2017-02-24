63c499a2518db5a56b3b8b55654ef02ee517ce8a
megwhetung <meg.whetung@gmail.com>
2017-02-24 09:37:25 -0800

    Updated text from Sarah

--- src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
+++ src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  retroYearsTitle: "Add previous years to your application (optional)",
+  retroYearsTitle: "Tell us which years you would like to apply for",
   continueButton: "Continue",
   docUploadInstruction: "Include a scan or photograph of your Notice of Assessement from Canada Revenue Agency for each year you have selected.",
-  applyForPreviousYearInstruction: "Be sure to get assistance for any previous years you may have qualified for - share your income tax information for up to six more years."
+  applyForPreviousYearInstruction: "Be sure to get assistance for any years you may have qualified for – share your Notice of Assessment or Reassessment from Canada Revenue Agency. Up to six years can be included."
 }
