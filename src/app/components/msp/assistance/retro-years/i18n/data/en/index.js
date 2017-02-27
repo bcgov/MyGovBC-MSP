9780bbf5617bb724e9830771a604b8943708aa77
megwhetung <meg.whetung@gmail.com>
2017-02-27 10:39:28 -0800

    add instruction re: including all pages for past years NOA
    Includes Sarah's edits

--- src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
+++ src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   retroYearsTitle: "Tell us which years you would like to apply for",
   continueButton: "Continue",
-  docUploadInstruction: "Include a scan or photograph of your Notice of Assessement from Canada Revenue Agency for each year you have selected.",
+  docUploadInstruction: "Include a scan or photograph of your Notice of Assessment for each year you have selected.<ul><li>Include all pages</li><li>Make sure your name is visible</li></ul>",
   applyForPreviousYearInstruction: "Be sure to get assistance for any years you may have qualified for – share your Notice of Assessment or Reassessment from Canada Revenue Agency. Up to six years can be included."
 }
