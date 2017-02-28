ec52e19d6f1589295f707910b0b3c067fc0155eb
megwhetung <meg.whetung@gmail.com>
2017-02-28 11:09:30 -0800

    shorten personal info tab name
    so that it doesn't break on small screens

--- src/app/components/msp/application/i18n/data/en/index.js
+++ src/app/components/msp/application/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   serviceName: 'Apply for MSP',
   progressStep1: 'Check Eligibility',
-  progressStep2: 'Personal Info & Documents',
+  progressStep2: 'Personal Info',
   progressStep3: 'Contact Info',
   progressStep4: 'Review & Submit'
 }
