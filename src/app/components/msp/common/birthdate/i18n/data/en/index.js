e318a70d5c3826956e5fb8674f777a98da8ace6a
Greg Turner <greg.fun@gmail.com>
2017-03-01 11:35:41 -0800

    fixed Applicant birthdate less than 16 years old not validated

--- src/app/components/msp/common/birthdate/i18n/data/en/index.js
+++ src/app/components/msp/common/birthdate/i18n/data/en/index.js
@@ -5,6 +5,16 @@ module.exports = {
   yearErrorRequired: 'Year is required',
   yearErrorBadFormat: 'This does not appear to be a valid date',
   yearErrorFutureCheck: 'Birth date can not be in the future',
-  yearErrorAgeCheck: 'A child must be less than 19 years old and a post-secondary student must be between 19 and 24 years',
 
+    /**
+     *   Applicant,
+     Spouse,
+     ChildUnder19,
+     Child19To24,
+     */
+  yearErrorAgeCheck: [
+      'An applicant must be 16 years or older',
+      '',
+      'A child must be less than 19 years old',
+      'A post-secondary student must be between 19 and 24 years']
 }
