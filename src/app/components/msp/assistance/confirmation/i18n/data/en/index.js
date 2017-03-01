7230dd120c3eb5a5cb9ed7c573dfb8d2b6c18ca5
megwhetung <meg.whetung@gmail.com>
2017-03-01 13:15:44 -0800

    PA - make print reference # prompt stronger
    as per MoH feedback - text provided by Sarah

--- src/app/components/msp/assistance/confirmation/i18n/data/en/index.js
+++ src/app/components/msp/assistance/confirmation/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   pageTitle: '<i class="fa fa-check success" aria-hidden="true"></i> Your application has been submitted',
   secondTitle: 'What happens next',
-  printEmailInstructions: '<a>Print this confirmation screen</a> for your records',
+  printEmailInstructions: '<strong>Important:</strong> Keep your reference number – write it down, or print this page for your records',
   emailAddressLabel: 'Email address',
   sendEmailButton: 'Send a copy',
   generalInstructions1: '<strong>If you qualify</strong>, the monthly premium you need to pay will be adjusted.  Depending on the level of assistance you\'re qualified for, you may still need to pay a small amount - a monthly invoice will be sent to you.<br><br>If there\'s a credit on your account, it will be applied to your monthly premium until the credit is used up.  A refund cheque will only be mailed to you if there\'s a credit on your account and you do not owe a monthly premium',
