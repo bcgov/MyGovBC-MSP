383ea37feed08ffaa4cd0108684ee80c3e91f944
Greg Turner <greg.fun@gmail.com>
2017-03-08 10:18:07 -0800

    Revert "experimenting with new domain name"
    This reverts commit 40abf81e526e25704945a7d385db6875869d704c.

--- config/webpack/global.js
+++ config/webpack/global.js
@@ -22,7 +22,7 @@ module.exports = function (_path) {
     output: {
       path: require("path").resolve("dist"),
       filename: '[name].js',
-      publicPath: '/msp',
+//      publicPath: '/',
     },
 
     // resolves modules
@@ -105,7 +105,7 @@ module.exports = function (_path) {
       new ExtractTextPlugin('assets/styles/css/[name]' + (NODE_ENV === 'development' ? '' : '.[chunkhash]') + '.css', {allChunks: true})
     ],
     devServer: {
-      publicPath: '/msp',
+      publicPath: '/',
       contentBase: './dist',
       info: true,
       hot: true,
