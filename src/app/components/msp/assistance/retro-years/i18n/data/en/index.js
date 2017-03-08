f10edf997231470a1bcaa4381bce1ba4a188ed30
megwhetung <meg.whetung@gmail.com>
2017-03-07 21:52:50 -0800

    fix typo

--- src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
+++ src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
@@ -3,5 +3,5 @@ module.exports = {
   continueButton: "Continue",
   docUploadInstruction: "Include a scan or photograph of your Notice of Assessment for each year you have selected.<ul><li>Include all pages</li><li>Make sure your name is visible</li></ul>",
   applyForPreviousYearInstruction: "Be sure to get assistance for any years you may have qualified for – share your Notice of Assessment or Reassessment from Canada Revenue Agency. Up to six years can be included.",
-  noaDocRequired: "Copies of your Notice of Assessement from CRA are required for each previous year assistance you are appluying for."
+  noaDocRequired: "Copies of your Notice of Assessement from CRA are required for each previous year assistance you are applying for."
 }
