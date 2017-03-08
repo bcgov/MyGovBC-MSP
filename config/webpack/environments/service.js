b6b40773dea1e685e0d1cdd03ed369c7645d02c2
Greg Turner <greg.fun@gmail.com>
2017-03-08 14:25:24 -0800

    new build and env vars for listening under /msp

--- config/webpack/environments/service.js
+++ config/webpack/environments/service.js
@@ -3,7 +3,7 @@ module.exports = function(_path) {
   return {
     appConstants: {
       serviceName: 'Apply for BC Health Care',
-      logBaseUrl: '/api/logging',
+      logBaseUrl: '/msp/api/logging',
       // to test logging api in your localhost dev env, don't change above config. Instead,
       // put following config entry in file ./local.js, which is ignored by git
       // logBaseUrl: 'http://logstash-gcpe-mygovbc-msp-dev.pathfinder.gov.bc.ca',
