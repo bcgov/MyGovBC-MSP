e28200471f2f4464360c7c526b9b88a622c446c7
Greg Turner <greg.fun@gmail.com>
2017-03-09 13:17:57 -0800

    fixed broken content file

--- src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
+++ src/app/components/msp/assistance/retro-years/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   retroYearsTitleDocsNotRequired: "Documents not required",
   retroYearsTitle: "Documents required",
-  docUploadInstructions: "<i class="fa fa-upload fa icon-upload"></i> Upload your documents",
+  docUploadInstructions: "<i class='fa fa-upload fa icon-upload'></i> Upload your documents",
   continueButton: "Continue",
   docUploadInstruction: "Scan the document, or take a photo of it. Make sure that it’s:<ul><li>The entire document, from corner to corner, with your name visible</li><li>At least 1000 pixels wide x 1500 pixels tal</li><li>Rotated correctly (not upside down or sideways)</li><li>In focus and easy to read</li><li>A JPG or PNG file (not a PDF)</li></ul>",
   applicantDocsRequired: "Your Notice of Assessment or Reassessment from Canada Revenue Agency for: <strong>{taxYearsAppliedFor}</strong>.",
