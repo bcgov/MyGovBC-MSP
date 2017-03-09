9c4644f39f7008bc222f05588c265da90783f243
michael <michaeltang1982@gmail.com>
2017-03-09 11:07:11 -0800

    tidy up of the eligibility-card.component.ts file

--- src/app/components/msp/assistance/prepare/eligibility-card/eligibility-card.component.ts
+++ src/app/components/msp/assistance/prepare/eligibility-card/eligibility-card.component.ts
@@ -1,21 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { DeductionCalculatorComponent } from
-   '../deduction-calculator/deduction-calculator.component';
 import {FinancialAssistApplication} from '../../../model/financial-assist-application.model';
-import {isNullOrUndefined} from "util";
-
-   
 @Component({
   selector: 'fin-assist-eligibility-card',
   templateUrl: './eligibility-card.html',
-
 })
 export class EligibilityCardComponent {
   lang = require('./i18n');
-  
   @Input() application: FinancialAssistApplication;
   @Input() editRouterLink: string;
-
   get _mainDisabilityCredit():number {
     return this.application.applicantDisabilityCredit;
   }
