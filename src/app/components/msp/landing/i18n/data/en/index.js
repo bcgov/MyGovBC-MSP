f11c3ab48891f8deff592cbc5891c4bb8f70bc13
Yiling Lu <yilingl@gmail.com>
2017-03-21 12:08:46 -0700

    Remove text line on landing page as user request

--- src/app/components/msp/landing/i18n/data/en/index.js
+++ src/app/components/msp/landing/i18n/data/en/index.js
@@ -1,4 +1,6 @@
 module.exports = {
-  newEnrollApp: 'Apply for MSP',
-  newPaApp: 'Apply for Premium Assistance',
+  newEnrollApp: 'New MSP enrollment application',
+  newPaApp: 'New MSP premium assistance application',
+  continueEnrollApp: 'Continue enrollment application',
+  continuePaApp: 'Continue premium assistance application'
 }
