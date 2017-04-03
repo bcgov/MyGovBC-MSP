7d23772fb000e36e6f7c8dae89d90545963d5e77
Yiling Lu <yilingl@gmail.com>
2017-04-03 11:04:17 -0700

    Use shorter UUID for images.

--- src/app/components/msp/model/application.model.ts
+++ src/app/components/msp/model/application.model.ts
@@ -43,8 +43,9 @@ class MspApplication implements ApplicationBase {
     let all = this.getAllImages();
     for(let i= 0; i < all.length; i++ ){
       let mspImage: MspImage = all[i];
-      let index = i + 1;
-      mspImage.setUUIDForImage(this._uuid + '-' + index + '-' + all.length);
+      mspImage.regenUUID();
+      // let index = i + 1;
+      // mspImage.setUUIDForImage(this._uuid + '-' + index + '-' + all.length);
     }
   }
   get applicant(): Person {
--- src/app/components/msp/model/financial-assist-application.model.ts
+++ src/app/components/msp/model/financial-assist-application.model.ts
@@ -55,9 +55,9 @@ export class FinancialAssistApplication implements ApplicationBase {
     let all = this.getAllImages();
     for(let i= 0; i < all.length; i++ ){
       let mspImage: MspImage = all[i];
-      // mspImage.regenUUID();
-      let index = i + 1;
-      mspImage.setUUIDForImage(this._uuid + '-' + index + '-' + all.length);
+      mspImage.regenUUID();
+      // let index = i + 1;
+      // mspImage.setUUIDForImage(this._uuid + '-' + index + '-' + all.length);
     }
   }
 
