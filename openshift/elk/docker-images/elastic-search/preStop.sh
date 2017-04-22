1078be9ac7df460b008d255112e02fbdc34900dc
f-w <fred.wen@gov.bc.ca>
2017-04-22 15:58:10 -0700

    updated cron to reset shard allocation filter

--- openshift/elk/docker-images/elastic-search/preStop.sh
+++ openshift/elk/docker-images/elastic-search/preStop.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 # get node id
 nodeId=$(curl -s localhost:9200/_nodes/_local/name?pretty|sed '4q;d'|sed -n 's/.*"\(.*\)".*/\1/p')
-# get current excluded ids
-excludedIds=$(curl -s localhost:9200/_cluster/settings?pretty|sed -n '/_id/{p;q;}'|sed -n 's/.*: "\(.*\)".*/\1/p')
-# update excluded ids variable
-excludedIds=$excludedIds,$nodeId
-# update excluded id settings
-curl -XPUT localhost:9200/_cluster/settings?pretty -H 'Content-Type: application/json' -d"
-{
-  \"transient\" : {
-    \"cluster.routing.allocation.exclude._id\" : \"$excludedIds\"
-  }
+
+addNodeToExclusionFilter(){
+  # get current excluded ids
+  excludedIds=$(curl -s localhost:9200/_cluster/settings?pretty|sed -n '/_id/{p;q;}'|sed -n 's/.*: "\(.*\)".*/\1/p')
+  # update excluded ids variable
+  if [[ $excludedIds != *$nodeId* ]]; then
+    excludedIds=$excludedIds,$nodeId
+    # update excluded id settings
+    curl -XPUT localhost:9200/_cluster/settings?pretty -H 'Content-Type: application/json' -d"
+    {
+      \"transient\" : {
+        \"cluster.routing.allocation.exclude._id\" : \"$excludedIds\"
+      }
+    }
+    "
+  fi
 }
-"
-docCnt=1
-while [ $docCnt -gt 0 ]
-do
-echo $docCnt
-sleep 2
-#get current doc count on local node
-docCnt=$(curl -s localhost:9200/_nodes/_local/stats/indices/docs?pretty|sed -n '/count/{p;q;}'|sed -n 's/.*\"count" :\(.*\),.*/\1/p')
+addNodeToExclusionFilter
+
+# wait for doc count goes down to 0
+while : ; do
+    docCnt=$(curl -s localhost:9200/_nodes/_local/stats/indices/docs?pretty|sed -n '/count/{p;q;}'|sed -n 's/.*\"count" :\(.*\),.*/\1/p')
+    echo $docCnt
+    [[ $docCnt -gt 0 ]] || break
+    addNodeToExclusionFilter
+    sleep 2
 done
