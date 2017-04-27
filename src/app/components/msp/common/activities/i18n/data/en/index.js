d935eef8854dbde2d23d5dbe56700625823a8d70
megwhetung <meg.whetung@gmail.com>
2017-04-27 09:40:13 -0700

    Update status sub-menu options
    re: PSPDN-736

--- src/app/components/msp/common/activities/i18n/data/en/index.js
+++ src/app/components/msp/common/activities/i18n/data/en/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   0: 'Not new to B.C. but need to apply for MSP',
-  1: 'Moving from another province',
-  2: 'Moving from another country',
+  1: 'Moved to B.C. from another province',
+  2: 'Moved to B.C. from another country',
   3: 'Working in B.C.',
   4: 'Studying in B.C.',
   5: 'Religious worker',
