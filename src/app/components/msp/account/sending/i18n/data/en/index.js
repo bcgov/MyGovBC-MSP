5907ec9ba48997d8540a8f4d6f7a05ba1b6ddfa4
saravan.kumar <saravan.kumar@maximusbc.ca>
2017-10-19 11:50:16 -0700

    paper form links updated

--- src/app/components/msp/account/sending/i18n/data/en/index.js
+++ src/app/components/msp/account/sending/i18n/data/en/index.js
@@ -4,6 +4,6 @@ module.exports = {
   transmissionInProgressMessage: 'Please do not close this browser tab or window, it can take 1-2 minutes to complete the submission.',
   transmissionErrorTitle: 'System error',
   instructionForUserOnErrorLine1: '<p>Sorry, it looks like something happened while your request was being submitted.</p><p>You can try submitting again - the data you&#8217;ve entered is saved on your device until you close the web browser.</p>',
-  tryToSumitAgain: 'Try to submit your request again',
-  instructionForUserOnErrorLine2: '<p>If that doesn&#8217;t work, apply using a paper form:</p><ul><li><a href="http://www2.gov.bc.ca/assets/gov/health/forms/102fil.pdf" target="_blank">MSP Application form (PDF)</a></li></ul>'
+  tryToSumitAgain: 'Try to submit your application again',
+  instructionForUserOnErrorLine2: '<p>If that doesn&#8217;t work, apply using a paper form:</p><ul><li><a href="https://www2.gov.bc.ca/assets/gov/health/forms/203fil.pdf" target="_blank">MSP Account Change form (PDF)</a></li></ul>'
 }
