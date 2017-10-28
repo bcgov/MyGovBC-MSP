a4340c7d3baf9bd7ab6252208e57370246bf0adc
saravan.kumar <saravan.kumar@maximusbc.ca>
2017-10-28 10:57:54 -0700

    faq link and other text changes

--- src/app/components/msp/account/prepare/i18n/data/en/index.js
+++ src/app/components/msp/account/prepare/i18n/data/en/index.js
@@ -1,12 +1,14 @@
 module.exports = {
-    pageTitle: 'What changes would you like to make to your MSP account?',
-    subTitle:'Medical Services Plan (MSP) Account Holders can use this web application to submit updates and personal documentation to keep your MSP account information current. You will be required to upload legal documents to verify this information.',
-    selectTitle:'Select all that apply',
+    pageTitle: 'What changes would you like to request for your MSP account?',
+    subTitle: 'MSP Account Holders can use this web application to request account changes, corrections and submit supporting documentation to keep your MSP account information current. You will be required to upload supporting documentation to verify this information.',
+    groupAccoungMsg: 'Do not use this web application if your MSP Premiums are paid by your employer or you are a member of a group account. Please contact your Group Administrator.',
+    faqMsg: "<a href='http://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card' target='blank'>If you have questions regarding supporting documentation or other frequently asked questions, please click link.</a>",
+    selectTitle: 'Select all that apply',
     addressChange: 'Address Update (Address Update must be done by the Account Holder)',
     consentProcessName: 'MSP Account Change Request',
     personalInfoChange: 'Personal Information Update (Name, Date of Birth, or Gender) for Account Holder, Spouse/Common-Law and/or Child',
     dependentChange: 'Add/Remove Spouse/Common-Law and/or Child(ren)',
     statusChange: 'Update Status in Canada (e.g., Permanent Resident)',
     continueButton: 'Continue',
-    addressRedirecMsg:'You will be redirected to Address Change BC to complete your request. Please complete the field below and click Continue.'
+    addressRedirecMsg: 'You will be redirected to Address Change BC to complete your request. Please complete the field below and click Continue.'
 }
