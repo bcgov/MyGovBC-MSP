c04acde9a47689c538f12325b3ea43bfbb03d71e
Adam Coard <acoard@advantcomp.com>
2017-10-30 11:00:38 -0700

    Toggle tests

--- src/app/components/msp/common/toggle/toggle.component.spec.ts
+++ src/app/components/msp/common/toggle/toggle.component.spec.ts
@@ -22,4 +22,25 @@ describe('ToggleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have no selection by defalt', () => {
+    expect(component.data).toBeUndefined();
+  });
+
+  it('should be invalid when selection is undefined', () => {
+    expect(component.isValid()).toBeFalsy();
+  });
+
+  it('should be valid when selection is true', () => {
+    component.data = true;
+    expect(component.isValid()).toBeTruthy();
+  });
+  it('should be valid when selection is false', () => {
+    component.data = false;
+    expect(component.isValid()).toBeTruthy();
+  });
+  it('should be invalid when selection is null', () => {
+    component.data = null;
+    expect(component.isValid()).toBeFalsy();
+  });
 });
