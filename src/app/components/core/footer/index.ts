5308cfbb2ab86623c3eeb34003c0d09fd5c73773
jam.hamidi <jam.hamidi@maximusbc.ca>
2017-11-22 10:43:19 -0800

    work in progress

--- src/app/components/core/footer/index.ts
+++ src/app/components/core/footer/index.ts
@@ -1,4 +1,9 @@
 import {Component, OpaqueToken, Inject} from '@angular/core'
+// import * as AssistSupport from './assist-support.js';
+
+declare var System: any;
+// declare var testClickedAssist: any;
+
 @Component({
   selector: 'core-footer',
   templateUrl: './index.html',
@@ -6,5 +11,15 @@ import {Component, OpaqueToken, Inject} from '@angular/core'
 })
 export class CoreFooterComponent {
   constructor() {
+  }
+
+  clickedAssist = () =>{
+    console.log ('clickedAssist');
+    System.import('./assist-support.js').then(AssistSupport=> {
+        (<any>window).AssistBoot.addAssistBehaviour();
+        (<any>window).AssistBoot.startAssistDialog();
+        // AssistSupport.testClickedAssist();
+        console.log("gi");
+    });
   }
 }
