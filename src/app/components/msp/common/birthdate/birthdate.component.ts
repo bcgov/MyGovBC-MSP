a463d7b08b467defbb6052f83d74ae59249261c4
saravan.kumar <saravan.kumar@maximusbc.ca>
2018-02-19 11:43:39 -0800

    birthdate doesnt store to session storage of its own.Possible fix for empty birthdate applications

--- src/app/components/msp/common/birthdate/birthdate.component.ts
+++ src/app/components/msp/common/birthdate/birthdate.component.ts
@@ -1,4 +1,13 @@
-import {Component, Input, ViewChild, Output, EventEmitter, AfterViewInit, OnInit, ChangeDetectorRef} from '@angular/core'
+import {
+    Component,
+    Input,
+    ViewChild,
+    Output,
+    EventEmitter,
+    AfterViewInit,
+    OnInit,
+    ChangeDetectorRef
+} from '@angular/core'
 import {NgForm} from "@angular/forms";
 import {Person} from "../../model/person.model";
 import {Relationship} from "../../model/status-activities-documents";
@@ -8,103 +17,112 @@ import {BaseComponent} from "../base.component";
 
 
 @Component({
-  selector: 'msp-birthdate',
-  templateUrl: './birthdate.component.html',
-  styleUrls: ['./birthdate.component.less']
-  
+    selector: 'msp-birthdate',
+    templateUrl: './birthdate.component.html',
+    styleUrls: ['./birthdate.component.less']
+
 })
 export class MspBirthDateComponent extends BaseComponent {
 
-  lang = require('./i18n');
+    lang = require('./i18n');
 
-  // Create today for comparison in check later
-  today:any;
-  @Input() isForAccountChange: boolean = false;
-  constructor(private cd: ChangeDetectorRef) {
-    super(cd);
-    this.today = moment();
-  }
+    // Create today for comparison in check later
+    today: any;
+    @Input() isForAccountChange: boolean = false;
 
-  @Input() person: Person;
-  @Input() showError: boolean;
-  @Output() onChange = new EventEmitter<any>();
-  @ViewChild('formRef') form: NgForm;
-
-  setYearValueOnModel(value:number){
-      this.person.dob_year = value;
-  }
-
-  setDayValueOnModel(value:number){
-      this.person.dob_day = value;
-  }
-  /**
-   * Determine if date of birth is valid for the given person
-   *
-   * @returns {boolean}
-   */
-  isCorrectFormat(): boolean {
-
-    // Validate
-    if (this.person &&
-        this.person.dob &&
-        this.person.dob.isValid()) {
-      return true;
-    }else{
-      return false;
+    constructor(private cd: ChangeDetectorRef) {
+        super(cd);
+        this.today = moment();
     }
 
-  }
+    @Input() person: Person;
+    @Input() showError: boolean;
+    @Output() onChange = new EventEmitter<any>();
+    @ViewChild('formRef') form: NgForm;
 
-  isInTheFuture(): boolean {
-    return this.person.dob.isAfter(this.today);
-  }
-  tooFarInThePast(): boolean {
-    let far = (this.today.get('y') - this.person.dob.get('y')) > 150;
-
-    return far;
-  }
-
-  ageCheck(): boolean {
-    // Applicant rules
-    if (this.person.relationship === Relationship.Applicant) {
-      // must be 16 or older for applicant
-      let tooYoung = this.person.dob.isAfter(moment().subtract(16, 'years'))
-      return !tooYoung;
+    ngAfterViewInit(): void {
+        this.form.valueChanges.subscribe(values => {
+            this.onChange.emit(values);
+        });
     }
-    // ChildUnder19 rules
-    else if (this.person.relationship === Relationship.ChildUnder19) {
-      // must be less than 19 if not in school
-      let lessThan19 = this.person.dob.isAfter(moment().subtract(19, 'years'))
-      return lessThan19;
-      
+
+    setYearValueOnModel(value: number) {
+        this.person.dob_year = value;
     }
-    // ChildUnder24 rules - Account Management Child
-    else if (this.person.relationship === Relationship.ChildUnder24) {
-        let tooOld = this.person.dob.isBefore(moment().subtract(24, 'years'));
-        return !tooOld;
+
+    setDayValueOnModel(value: number) {
+        this.person.dob_day = value;
+    }
+
+    /**
+     * Determine if date of birth is valid for the given person
+     *
+     * @returns {boolean}
+     */
+    isCorrectFormat(): boolean {
+
+        // Validate
+        if (this.person &&
+            this.person.dob &&
+            this.person.dob.isValid()) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
-    else if (this.person.relationship === Relationship.Child19To24) {
-      // if child student must be between 19 and 24
 
-      let tooYoung = this.person.dob.isAfter(moment().subtract(19, 'years'));
-      let tooOld = this.person.dob.isBefore(moment().subtract(24, 'years'));
-
-      if(tooYoung){
-        // console.log('This child is less than 19 years old.')
-      }
-      if(tooOld){
-        // console.log('This child is older than 24.')
-      }
-      let ageInRange = !tooYoung && !tooOld;
-      return ageInRange;
-    }else{
-      return true;
+    isInTheFuture(): boolean {
+        return this.person.dob.isAfter(this.today);
     }
-  }
 
-  isValid():boolean {
-    return this.isCorrectFormat() && !this.isInTheFuture() && !this.tooFarInThePast() && this.ageCheck();
-  }
+    tooFarInThePast(): boolean {
+        let far = (this.today.get('y') - this.person.dob.get('y')) > 150;
+
+        return far;
+    }
+
+    ageCheck(): boolean {
+        // Applicant rules
+        if (this.person.relationship === Relationship.Applicant) {
+            // must be 16 or older for applicant
+            let tooYoung = this.person.dob.isAfter(moment().subtract(16, 'years'))
+            return !tooYoung;
+        }
+        // ChildUnder19 rules
+        else if (this.person.relationship === Relationship.ChildUnder19) {
+            // must be less than 19 if not in school
+            let lessThan19 = this.person.dob.isAfter(moment().subtract(19, 'years'))
+            return lessThan19;
+
+        }
+        // ChildUnder24 rules - Account Management Child
+        else if (this.person.relationship === Relationship.ChildUnder24) {
+            let tooOld = this.person.dob.isBefore(moment().subtract(24, 'years'));
+            return !tooOld;
+
+        }
+        else if (this.person.relationship === Relationship.Child19To24) {
+            // if child student must be between 19 and 24
+
+            let tooYoung = this.person.dob.isAfter(moment().subtract(19, 'years'));
+            let tooOld = this.person.dob.isBefore(moment().subtract(24, 'years'));
+
+            if (tooYoung) {
+                // console.log('This child is less than 19 years old.')
+            }
+            if (tooOld) {
+                // console.log('This child is older than 24.')
+            }
+            let ageInRange = !tooYoung && !tooOld;
+            return ageInRange;
+        } else {
+            return true;
+        }
+    }
+
+    isValid(): boolean {
+        return this.isCorrectFormat() && !this.isInTheFuture() && !this.tooFarInThePast() && this.ageCheck();
+    }
 
 }
