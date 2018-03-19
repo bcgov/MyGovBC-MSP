6ff4a7b2a4c7c8a47e9188c83aa2a86d7622d870
saravan.kumar <saravan.kumar@maximusbc.ca>
2018-03-19 14:54:29 -0700

    Fixing Tests

--- src/app/components/msp/common/image-error-modal/image-error-modal.component.spec.ts
+++ src/app/components/msp/common/image-error-modal/image-error-modal.component.spec.ts
@@ -3,14 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { MspDataService } from '../../service/msp-data.service';
 import { LocalStorageService, LocalStorageModule } from 'angular-2-local-storage';
 import {RouterTestingModule} from "@angular/router/testing";
-
+import { ModalModule } from "ngx-bootstrap";
 import {MspImageErrorModalComponent} from "./image-error-modal.component";
 
 describe('MspImageErrorModalComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [MspImageErrorModalComponent],
-      imports: [FormsModule, RouterTestingModule,LocalStorageModule.withConfig({
+      imports: [FormsModule, RouterTestingModule,ModalModule.forRoot(),LocalStorageModule.withConfig({
         prefix: 'ca.bc.gov.msp',
         storageType: 'sessionStorage'
       })],
