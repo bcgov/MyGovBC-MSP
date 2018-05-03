f87a32e7009d80d5416d3da67fa710104105b4b2
saravan.kumar <saravan.kumar@maximusbc.ca>
2018-05-03 15:42:48 -0700

    date validations correctd for departure and return date components

--- src/app/components/msp/common/return-date/i18n/data/en/index.js
+++ src/app/components/msp/common/return-date/i18n/data/en/index.js
@@ -8,5 +8,8 @@ module.exports = {
     yearErrorRequired: 'Year is required',
     yearErrorBadFormat: 'This does not appear to be a valid date',
     yearErrorFutureCheck: 'Return date can not be in the future',
-    datesOutOfOrder: "Please provide a return date that is <em>later</em> than the departure date."
+    datesOutOfOrder: "Please provide a return date that is <em>later</em> than the departure date.",
+    yearErrorDistantPast: 'Date is too far in the past',
+    yearErrorDistantFuture: 'Date is too far in the future',
+    calendarDayOutOfRange: 'Day of the month is out of range'
 }
