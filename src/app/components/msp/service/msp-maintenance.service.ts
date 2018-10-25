5a69a7bc6e9a6bedef459f4a476821c0876a88a6
Jam <jam.hamidi@maximusbc.ca>
2018-10-25 11:31:08 -0700

    removing jxon and few minor changes

--- src/app/components/msp/service/msp-maintenance.service.ts
+++ src/app/components/msp/service/msp-maintenance.service.ts
@@ -5,7 +5,6 @@ import { ApplicationTypeFactory, AttachmentType, AttachmentTypeFactory, Attachme
 import { ResponseType } from "../api-model/responseTypes";
 import {ISpaEnvResponse} from '../model/spa-env-response.interface';
 import { MspLogService } from './log.service';
-const jxon = require('jxon/jxon');
 
 
 @Injectable()
@@ -30,28 +29,32 @@ export class MspMaintenanceService  {
             
             // Setup headers
             let headers = new HttpHeaders({
+                'program': 'msp',
+                'times': '',
+                'method': 'checkMaintenance',
+                'severity': 'info',
                 'SPA_ENV_NAME': envName
             });
 			
 			// Setting the options for the rest call 
-            let options = {headers: headers,responseType: "text" as "text"};
+            let options = {headers: headers, responseType: "text" as "text"};
             return this.http.post(url, null, options)
                 .toPromise()
                 .then((response: string) => {
-                        console.log("SPA Environment Response "+response);
+                        console.log("SPA Environment Response " + response);
                         //return resolve(JSON.parse(response));
                         spaResponse = <ISpaEnvResponse> JSON.parse(response);
                         return resolve(spaResponse);
                     },
                     (error: Response | any) => {
-                        console.log('Unable to get the proper response from the SPA Maintenance API: ', error);
+                        console.log('Cannot get maintenance flag: ', error);
                         return resolve(error);
                     }
                 )
                 .catch((error: Response | any) => {
                     console.log("Error when calling the MSP Maintenance: ", error);
                     this.logService.log({
-                        text: "Error when calling the Maintenance Service. Unable to connect to the API",
+                        text: "Cannot connect to spa-env-server",
                         response: error,
                     }, "")
                     return resolve(error);
