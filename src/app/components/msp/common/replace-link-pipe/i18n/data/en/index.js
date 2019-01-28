7da348c6a628d0d8237da91f060048855b1e8e14
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2019-01-28 14:58:27 -0800

    Added the link for ACBC text

--- src/app/components/msp/common/replace-link-pipe/i18n/data/en/index.js
+++ src/app/components/msp/common/replace-link-pipe/i18n/data/en/index.js
@@ -4,6 +4,10 @@ module.exports = {
     {
       code: 'HIBC',
       name: ' <a href=\'https://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents-contact-us\' target=\'_blank\'> Health Insurance BC</a>'
+    },
+    {
+      code: 'ACBC',
+      name: ' <a href=\'https://www.addresschange.gov.bc.ca\' target=\'_blank\'> Change of Address Service</a>'
     }
 
   ]
