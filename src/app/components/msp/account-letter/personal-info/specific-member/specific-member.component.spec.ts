3056fd40c62c2335e0892d83951c05b8afca526d
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2019-02-01 12:06:18 -0800

    Fixed specific member , MSP ACl api and Maintenance API test cases

--- src/app/components/msp/account-letter/personal-info/specific-member/specific-member.component.spec.ts
+++ src/app/components/msp/account-letter/personal-info/specific-member/specific-member.component.spec.ts
@@ -2,13 +2,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpecificMemberComponent } from './specific-member.component';
 
+import {MspPhnComponent} from '../../../common/phn/phn.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProcessService } from '../../../service/process.service';
+import { MspACLService } from '../../../service/msp-acl-api.service';
+import { MspDataService } from '../../../service/msp-data.service';
+import { MspLogService } from '../../../service/log.service';
+import {Mod11CheckValidator} from '../../../common/phn/phn.validator';
+import {TextMaskModule} from 'angular2-text-mask';
+
+
 describe('SpecificMemberComponent', () => {
   let component: SpecificMemberComponent;
   let fixture: ComponentFixture<SpecificMemberComponent>;
+  
+  let validator: Mod11CheckValidator;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpecificMemberComponent ]
+      declarations: [ Mod11CheckValidator, SpecificMemberComponent, MspPhnComponent ],
+      imports: [FormsModule, RouterTestingModule, TextMaskModule],
+      providers: [MspDataService, MspLogService, ProcessService, MspACLService]
     })
     .compileComponents();
   }));
--- src/app/components/msp/service/msp-acl-api.service.spec.ts
+++ src/app/components/msp/service/msp-acl-api.service.spec.ts
@@ -7,8 +7,8 @@ import {
     ResponseOptions,
     Http
 } from "@angular/http";
-import {TestBed, getTestBed, inject} from '@angular/core/testing';
-import {MspMaintenanceService} from './msp-maintenance.service';
+import {TestBed, getTestBed, fakeAsync, tick, inject} from '@angular/core/testing';
+import {MspACLService} from './msp-acl-api.service';
 import {MspLogService} from './log.service';
 import {MspDataService} from './msp-data.service';
 import { LocalStorageService, LocalStorageModule } from 'angular-2-local-storage';
@@ -18,10 +18,13 @@ import {FormsModule} from '@angular/forms';
 
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import {ISpaEnvResponse} from '../model/spa-env-response.interface';
+import { AccountLetterApplication } from '../model/account-letter-application.model';
+import { AccountLetterApplicantTypeFactory, AccountLetterType  } from '../api-model/accountLetterTypes';
 
-describe('MspMaintenanceService', () => {
+
+describe('MspACLService', () => {
     let injector: TestBed;
-    let service: MspMaintenanceService;
+    let service: MspACLService;
     let httpMock: HttpTestingController;
     let spaEnvRes: ISpaEnvResponse;
 
@@ -34,25 +37,16 @@ describe('MspMaintenanceService', () => {
               prefix: 'ca.bc.gov.msp',
               storageType: 'sessionStorage'
           }) ],
-        providers: [ LocalStorageService , MspMaintenanceService, MspLogService, MspDataService]
+        providers: [ LocalStorageService , MspACLService, MspLogService, MspDataService]
       });
-      injector = getTestBed();
-      service = injector.get(MspMaintenanceService);
-      httpMock = injector.get(HttpTestingController);
+       injector = getTestBed();
+       service = injector.get(MspACLService);
+       httpMock = injector.get(HttpTestingController);
     });
 
-    it('should be created', inject([MspMaintenanceService], (service: MspMaintenanceService) => {
+    it('should be created', inject([MspACLService], (service: MspACLService) => {
       expect(service).toBeTruthy();
-    })); 
-   /* it('Calling the Maintenance API', () => {
-        const dummyResponse = [
-          {"SPA_ENV_MSP_MAINTENANCE_FLAG":"false", "SPA_ENV_MSP_MAINTENANCE_MESSAGE":"MSP System will be down from Time A to Time B"}];
-        this.spaEnvRes = service.checkMaintenance();
-        const req = httpMock.expectOne('/env');
-        expect(req.request.method).toBe("POST");
-        req.flush(dummyResponse);
-      });
-      */
+    }));
 
   });
 
