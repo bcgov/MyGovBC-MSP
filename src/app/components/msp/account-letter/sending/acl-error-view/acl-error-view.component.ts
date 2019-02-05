1dd059bdf99ea04a034740e1485a7681b87640cd
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2019-02-04 16:43:52 -0800

    Added the logging for SPA ENV rapid message

--- src/app/components/msp/account-letter/sending/acl-error-view/acl-error-view.component.ts
+++ src/app/components/msp/account-letter/sending/acl-error-view/acl-error-view.component.ts
@@ -7,6 +7,8 @@ import {MspLogService} from "../../../service/log.service";
 import {Router} from "@angular/router";
 import {ProcessService} from "../../../service/process.service";
 import {BaseComponent} from "../../../common/base.component";
+import {HttpErrorResponse} from '@angular/common/http';
+
 
 @Component({
     selector: 'msp-acl-error-view',
@@ -19,12 +21,19 @@ export class AclErrorViewComponent extends BaseComponent{
     @Input('response')response: any;
     public rapidError: string;
 
-    constructor(private aclService: MspACLService, private cd: ChangeDetectorRef ,private router: Router) {
+    constructor(private aclService: MspACLService, private cd: ChangeDetectorRef ,private router: Router, private logService: MspLogService) {
         super(cd);
         if (this.response && this.response.rapidResponse) {
             this.aclService
                 .sendSpaEnvServer(this.response.rapidResponse)
                 .subscribe(response => {
+                    
+                    if (response instanceof HttpErrorResponse) { // probable network errors..Spa Env server could be down
+                        this.logService.log({
+                            name: 'ACL - SPA Env System Error'
+                        }, 'ACL - SPA Env Rapid Response Error' + response.message);
+                        return;
+                    }
                     console.log(response);
                     this.rapidError =  response.message;
                 });
