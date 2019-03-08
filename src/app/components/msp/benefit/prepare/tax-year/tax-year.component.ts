181ada9ce145a6de026115eac34c94638ecf98b9
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2019-03-08 14:08:31 -0800

    Fixed the error related to the cutoff date

--- src/app/components/msp/benefit/prepare/tax-year/tax-year.component.ts
+++ src/app/components/msp/benefit/prepare/tax-year/tax-year.component.ts
@@ -62,11 +62,11 @@ export class TaxYearComponent extends BaseComponent {
                 assistYear.year = yearNum;
                 assistYear.docsRequired = true;
                 assistYear.currentYear = this.benefitApp.MostRecentTaxYear;
-                console.log('cutoffDate'+this.cutOffDate().toDate()) ;
-                console.log('today  '+this.today.toDate());
+                //console.log('cutoffDate'+this.cutOffDate().toDate()) ;
+                //console.log('today  '+this.today.toDate());
 
                 // checking the cutoff Date and disabling the last year
-                if(this.cutOffDate().isSameOrBefore(this.today) && assistYear.year == this.benefitApp.MostRecentTaxYear - 1) {
+                if(this.cutOffDate() && this.cutOffDate().isSameOrBefore(this.today) && assistYear.year == this.benefitApp.MostRecentTaxYear - 1) {
                     assistYear.disabled =  true; //  (assistYear.year == this.benefitApp.MostRecentTaxYear - 1) ? true : false;
                 } else {
                     assistYear.disabled =  false;
@@ -83,19 +83,23 @@ export class TaxYearComponent extends BaseComponent {
 
     // Calculates the cutoff date  
     cutOffDate() {
-        console.log(this.spaEnvResponse);
-        //this.suppBenefitCutOffDate = '2019-11-01';
-        const myDate = moment(this.spaEnvResponse.SPA_ENV_PACUTOFF_MAINTENANCE_START, 'YYYY-MM-DD HH:mm:ss').toDate();
-        console.log('*******'+myDate.getUTCMonth()+'====='+myDate.getUTCDate());
-       // let dateString = this.suppBenefitCutOffDate; 
-       // let newDate1 = new Date(dateString);
-       // console.log(this.suppBenefitCutOffDate);
-        console.log(myDate);
-        return moment({
-          year: myDate.getFullYear(),
-          month: myDate.getUTCMonth() + 1 , //this.spaEnvMonth - 1 , // moment use 0 index for month, so adding - 1 to get the correct month:(
-          day: myDate.getUTCDate() //this.spanEnvDay,
-        }).utc();
+        if(this.spaEnvResponse && this.spaEnvResponse.SPA_ENV_PACUTOFF_MAINTENANCE_START) {
+            console.log(this.spaEnvResponse);
+            //this.suppBenefitCutOffDate = '2019-11-01';
+            const myDate = moment(this.spaEnvResponse.SPA_ENV_PACUTOFF_MAINTENANCE_START, 'YYYY-MM-DD HH:mm:ss').toDate();
+            console.log('*******'+myDate.getUTCMonth()+'====='+myDate.getUTCDate());
+           // let dateString = this.suppBenefitCutOffDate; 
+           // let newDate1 = new Date(dateString);
+           // console.log(this.suppBenefitCutOffDate);
+            console.log(myDate);
+            return moment({
+              year: myDate.getFullYear(),
+              month: myDate.getUTCMonth() + 1 , //this.spaEnvMonth - 1 , // moment use 0 index for month, so adding - 1 to get the correct month:(
+              day: myDate.getUTCDate() //this.spanEnvDay,
+            }).utc();
+
+        } else {return ;}
+       
     }
 
 }
