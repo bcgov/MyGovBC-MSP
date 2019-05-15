267f10371983159cf4a6d2b7e970a8e206160c62
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2019-05-15 10:41:58 -0700

    Updated the Declaration for PA app

--- src/app/components/msp/assistance/authorize-submit/i18n/data/en/index.js
+++ src/app/components/msp/assistance/authorize-submit/i18n/data/en/index.js
@@ -6,11 +6,11 @@ module.exports = {
   agreeLabel: 'Yes, I agree',
   attorneyDoYouAgreeLabel: 'I have power of attorney and I am applying on behalf of {applicantName}',
   continueButton: 'Continue',
-  declaration: "I hereby consent to the release of information from my income tax returns, and other taxpayer information, by the Canada Revenue Agency to the Ministry of Health and/or Health Insurance BC. The information obtained will be relevant to and used solely for the purpose of determining and verifying my initial and ongoing entitlement to the premium assistance program under the Medicare Protection Act, and/or other income-based programs, including Supplementary Benefits, Emergency Health Services and Healthy Kids. \
+  declaration: "I hereby consent to the release of information from my income tax returns, and other taxpayer information, by the Canada Revenue Agency to the Ministry of Health and/or Health Insurance BC. The information obtained will be relevant to and used for the purpose of determining and verifying my initial and ongoing entitlement to the Premium Assistance Program and the Supplementary Benefits Program under the <i>Medicare Protection Act </i>, and will not be disclosed to any other party. \
   \
-  This authorization is valid for the taxation year prior to the signature of this application, the year of the signature and for each subsequent consecutive taxation year for which premium assistance is requested. It may be revoked by sending a written notice to Health Insurance BC.\
+  This authorization is valid for the taxation year prior to the signature of this application, the year of the signature and for each subsequent consecutive taxation year for which premium assistance and supplementary benefits is requested. It may be revoked by sending a written notice to Health Insurance BC.  <br/> <br/>\
   \
-  I am a resident of British Columbia as defined by the Medicare Protection Act.\
+  I am a resident of British Columbia as defined by the <i>Medicare Protection Act</i>.<br/> <br/>\
   \
-  I have resided in Canada as a Canadian citizen or holder of permanent resident status (landed immigrant) for at least the last 12 months immediately preceding this application; I am not exempt from liability to pay income tax by reason of any other Act; and I am not the child of another beneficiary."
+  I have resided in Canada as a Canadian citizen or holder of permanent resident status (landed immigrant) for at least the last 12 months immediately preceding this application. I am not exempt from liability to pay income tax by reason of any other Act."
 }
