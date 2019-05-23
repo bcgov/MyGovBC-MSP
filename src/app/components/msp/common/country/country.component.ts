2918b9fa9a316e7364aae32666fd3ee683c2b057
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-05-23 15:44:23 -0700

    updated the country form validation logic to properly evaluate whether a valid country has been selected based on the list

--- src/app/components/msp/common/country/country.component.ts
+++ src/app/components/msp/common/country/country.component.ts
@@ -1,14 +1,20 @@
-import {Component, Input, Output, EventEmitter, ViewChild, ChangeDetectorRef} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ChangeDetectorRef
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
-import {BaseComponent} from '../base.component';
+import { BaseComponent } from '../base.component';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'msp-country',
   templateUrl: './country.component.html'
 })
-
 export class MspCountryComponent extends BaseComponent {
   lang = require('./i18n');
 
@@ -22,44 +28,51 @@ export class MspCountryComponent extends BaseComponent {
   @Output() onChange = new EventEmitter<string>();
   @ViewChild('formRef') form: NgForm;
 
-  countryData: Array<{code: string, name: string}> = this.lang('./en/index.js').countryData;
+  countryData: Array<{ code: string; name: string }> = this.lang(
+    './en/index.js'
+  ).countryData;
 
-  constructor( private cd: ChangeDetectorRef) {
+  constructor(private cd: ChangeDetectorRef) {
     super(cd);
   }
 
   ngAfterViewInit(): void {
-    this.form.valueChanges.pipe(debounceTime(0), distinctUntilChanged()).subscribe((values) => {
-        if (this.country && this.country.length > 0) {
-            this.onChange.emit(this.country.trim());
+    this.form.valueChanges
+      .pipe(
+        debounceTime(0),
+        distinctUntilChanged()
+      )
+      .subscribe(values => {
+        const res = this.countryData.filter(itm => itm.name === this.country);
+        if (this.country && this.country.length > 0 && res.length > 0) {
+          this.onChange.emit(this.country.trim());
         }
-    });
+      });
   }
 
-
-    // to handle user typing a non-dropdown value..emit event.. used in mailing address where province cant be a drop down item for non-canada countires
-    typeaheadNoResults(event: boolean): void {
-        if (event) {
-       //     this.country =  this.country.slice(0, 25);
-            this.onChange.emit(this.country.trim());
-        }
+  // to handle user typing a non-dropdown value..emit event.. used in mailing address where province cant be a drop down item for non-canada countires
+  typeaheadNoResults(event: boolean): void {
+    if (event) {
+      //     this.country =  this.country.slice(0, 25);
+      this.onChange.emit(this.country.trim());
     }
+  }
 
-    updateModel(event: TypeaheadMatch	): void {
-        if (event && event.item) {
-            const eventVal: string = event.item['name'].trim().slice(0, 25);
-            this.country = eventVal;
-            this.onChange.emit(eventVal);
-        }
+  updateModel(event: TypeaheadMatch): void {
+    if (event && event.item) {
+      const eventVal: string = event.item['name'].trim().slice(0, 25);
+      this.country = eventVal;
+      this.onChange.emit(eventVal);
     }
+  }
 
-    isValid(): boolean {
-       if (this.country && this.country.trim().length > 0 ) {
-          return true;
-        } else {
-          return false;
-        }
-    
+  isValid(): boolean {
+    const res = this.countryData.filter(itm => itm.name === this.country);
+
+    if (this.country && this.country.trim().length > 0 && res.length > 0) {
+      return true;
+    } else {
+      return false;
     }
-
+  }
 }
