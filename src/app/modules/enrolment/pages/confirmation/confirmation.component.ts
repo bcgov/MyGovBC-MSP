7b7c98f93a3f20204572d1b7c2d3f123982ca683
Abhimanyu raut <39009744+AbhimanyuRaut@users.noreply.github.com>
2019-06-10 12:37:23 -0700

    New desgign updates to the review, confirmation and personal info page

--- src/app/modules/enrolment/pages/confirmation/confirmation.component.ts
+++ src/app/modules/enrolment/pages/confirmation/confirmation.component.ts
@@ -3,6 +3,7 @@ import {MspApplication} from '../../../../components/msp/model/application.model
 import { MspDataService } from '../../../../components/msp/service/msp-data.service';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Observable,  Subscription} from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   templateUrl: './confirmation.component.html'
@@ -25,6 +26,14 @@ export class ConfirmationComponent implements OnInit{
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  /**
+   * Today's date
+   * @returns {string}
+   */
+  get dateStamp(): string {
+    return moment().format('MMMM DD, YYYY');
   }
 
 }
