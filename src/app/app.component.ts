28778b24869d5020782341104d52ef05834cfa2b
Adam Coard <arcoard+github@gmail.com>
2019-06-10 12:15:07 -0400

    Remove body padding from main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { environment } from '../environments/environment';
 
 @Component({
   selector: 'general-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+
 })
 export class GeneralAppComponent {
   private viewContainerRef: ViewContainerRef;
