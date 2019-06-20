18be1f74be0604d9756d1fd6b10750cc51f5e760
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-06-20 13:42:10 -0700

    added documents subheading and collating the values of the tax years selected

--- src/app/modules/assistance/pages/personal-info/personal-info.component.ts
+++ src/app/modules/assistance/pages/personal-info/personal-info.component.ts
@@ -14,6 +14,7 @@ import { BaseComponent } from '../../../../models/base.component';
 import { MspAddressComponent } from '../../../msp-core/components/address/address.component';
 import { MspPhoneComponent } from '../../../../components/msp/common/phone/phone.component';
 import { FinancialAssistApplication } from '../../models/financial-assist-application.model';
+import { AssistanceYear } from '../../models/assistance-year.model';
 
 @Component({
   // templateUrl: './personal-info.component.html'
@@ -31,6 +32,10 @@ import { FinancialAssistApplication } from '../../models/financial-assist-applic
           [person]="financialAssistApplication.applicant"
         ></msp-assist-account-holder>
       </form>
+    </common-page-section>
+    <common-page-section>
+      <h3>{{ documentsTitle }}</h3>
+      <p>{{ documentsDescription }}</p>
     </common-page-section>
   `
 })
@@ -51,6 +56,11 @@ export class AssistancePersonalInfoComponent extends BaseComponent {
   subtitle = 'Account Holder (Main Applicant)';
   description =
     'Enter your legal name as it appears on your official Canadian identity documents, e.g., birth certificate, permanent resident card, passport.';
+  documentsTitle = 'Documents';
+  documentsDescription =
+    'Upload your Notice of Assessment (NOA) or Notice of Reassessment (NORA) from Canada Revenue Agency for ';
+
+  assistanceYears: any[];
   constructor(
     private dataService: MspDataService,
     private _router: Router,
@@ -81,6 +91,26 @@ export class AssistancePersonalInfoComponent extends BaseComponent {
 
   ngOnInit() {
     //  this.initProcessMembers(AssistancePersonalInfoComponent.ProcessStepNum, this._processService);
+    const assistYears = this.financialAssistApplication.assistYears;
+    let arr = [];
+    const checkYear = (year: AssistanceYear) => {
+      return year.apply ? year : null;
+    };
+    for (let year of assistYears) {
+      arr.push(checkYear(year));
+    }
+    this.assistanceYears = arr.filter(itm => itm != null);
+
+    this.documentsDescription += this.assistanceYears
+      .map((itm, i, arr) => itm.year)
+      .sort((a, b) => a - b)
+      .reduce((a, b, i, arr) =>
+        i === arr.length - 1 ? `${a} and ${b}.` : `${a}, ${b}`
+      );
+
+    // this.documentsDescription += '.';
+
+    console.log('arr', this.documentsDescription);
   }
 
   onChange($event) {
