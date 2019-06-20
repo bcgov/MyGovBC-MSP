dfbc2f0be338fa48ee7d4f5021ce1e66903c7109
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-06-19 17:24:27 -0700

    modal formatting and input now completed

--- src/app/modules/assistance/components/assist-rates-helper-modal/assist-rates-helper-modal.component.ts
+++ src/app/modules/assistance/components/assist-rates-helper-modal/assist-rates-helper-modal.component.ts
@@ -8,14 +8,23 @@ import { Observable, of, Subject } from 'rxjs';
     <h4>What year do you want to see the MSP premium rates for?</h4>
     <select (change)="selectYear($event)">
       <ng-container *ngIf="yearOptions$ | async as opts">
+        <option selected disabled>Year</option>
         <option *ngFor="let year of opts" (change)="selectYear(year)">{{
           year
         }}</option>
       </ng-container>
     </select>
 
-    <ng-container *ngIf="selectedYear$ | async as selected">
-      zzz
+    <div *ngIf="selectedYear$ | async as selected" class="p-3">
+      <br />
+      <div *ngIf="yearTitle$ | async as title" class="row pt-2">
+        <span class="col-12">
+          <b>{{ title }}</b>
+        </span>
+      </div>
+      <div *ngIf="taxReturnInfo$ | async as returnInfo" class="row pb-2">
+        <span class="col-12">{{ returnInfo }} </span>
+      </div>
 
       <table class="table table-bordered">
         <tr>
@@ -30,18 +39,19 @@ import { Observable, of, Subject } from 'rxjs';
           <th scope="col">{{ item.threeFamily | currency }}</th>
         </tr>
       </table>
-    </ng-container>
+    </div>
   `,
   styleUrls: ['./assist-rates-helper-modal.component.scss']
 })
 export class AssistRatesHelperModalComponent implements OnInit {
   @Input() rateData: any;
-
-  yearOptions: number[] = [];
   yearOptions$: Observable<number[]>;
   selectedYear$: Subject<IRateBracket[]> = new Subject();
   yearTitle$: Observable<string>;
-
+  yearSummary$: Observable<string>;
+  // Provide income information(CRA Notice of Assessment or Notice of Reassessment) for the tax year
+  incomeThreshold$: Observable<string>;
+  taxReturnInfo$: Observable<string>;
   tableHeaders = [
     'Adjusted Net Income',
     'One Person',
@@ -58,21 +68,28 @@ export class AssistRatesHelperModalComponent implements OnInit {
       opts.push(parseInt(key));
     }
     opts.sort((a, b) => b - a);
-    console.log(this.yearOptions);
+    // console.log(this.yearOptions);
     this.yearOptions$ = of(opts);
     this.selectedYear$.subscribe(obs => console.log(obs));
   }
 
   selectYear(event: any) {
     const year = event.target.value;
-    const yearTitle = `${year}(January 1, ${year} to December 31, ${year})`;
-    this.yearTitle$ = of(yearTitle);
     const selectedYear = this.rateData[year];
+    const yearTitle = `${year}(January 1, ${year} to December 31, ${year})`;
+    const taxReturnInfo = `Provide income information(CRA Notice of Assessment or Notice of Reassessment) for the ${year -
+      1} tax year`;
+    this.yearTitle$ = of(yearTitle);
     const arr = [];
     for (const key of Object.keys(selectedYear)) {
       arr.push(selectedYear[key]);
     }
     this.selectedYear$.next(arr);
+    const incomeThreshold = `Adjusted net income threshold for Retroactive Premiums Assistance: ${
+      arr[arr.length - 1].netIncome
+    }`;
+    this.incomeThreshold$ = of(incomeThreshold);
+    this.taxReturnInfo$ = of(taxReturnInfo);
     console.log('array', arr);
   }
 }
