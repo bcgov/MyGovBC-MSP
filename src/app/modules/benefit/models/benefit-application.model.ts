c4db7c279950c437e2927bbfe142872486252dec
Abhimanyu raut <39009744+AbhimanyuRaut@users.noreply.github.com>
2019-06-22 18:47:55 -0700

    Updated the Document API call

--- src/app/modules/benefit/models/benefit-application.model.ts
+++ src/app/modules/benefit/models/benefit-application.model.ts
@@ -352,7 +352,7 @@ export class BenefitApplication implements ApplicationBase {
      * Power of atterney docs and attendant care expense receipts
      */
     getAllImages(): MspImage[] {
-        return [...this.powerOfAttorneyDocs, ...this.attendantCareExpenseReceipts, ...this.assistYearDocs];
+        return [...this.powerOfAttorneyDocs, ...this.attendantCareExpenseReceipts, ...this.assistYearDocs, ...this.applicant.assistYearDocs, ...this.spouse.assistYearDocs];
     }
 
     /**
