b33811589303a792754b72c18b655c07a19b26f9
Adam Coard <arcoard+github@gmail.com>
2019-07-02 16:08:29 -0400

    Implement version.js + console log

---
+++ src/version.GENERATED.ts
@@ -0,0 +1,9 @@
+// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
+export const gitCommit = 'cfe2c17d';
+export const buildTime = '7/2/2019 at 1:06:59 PM';
+/** App version retrieved from package.json. */
+export const projectVersion = '1.1.0';
+/** Human readable message  */
+export const message = 'Canonical Version: cfe2c17d - 7/2/2019 at 1:06:59 PM (v1.1.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/cfe2c17d';
+/** If true, other values should be present. */
+export const success = true;
