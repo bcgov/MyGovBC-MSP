fe157b904524915935bc133c84b2d8d6c8657c59
Adam Coard <arcoard+github@gmail.com>
2019-07-04 18:29:56 -0400

    Implement common-xicon-button instead of arbitrary icon

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '691be682';
-export const buildTime = '7/4/2019 at 8:38:30 AM';
+export const gitCommit = 'fe9b6e2e';
+export const buildTime = '7/4/2019 at 3:19:19 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '1.1.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 691be682 - 7/4/2019 at 8:38:30 AM (v1.1.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/691be682';
+export const message = 'Canonical Version: fe9b6e2e - 7/4/2019 at 3:19:19 PM (v1.1.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/fe9b6e2e';
 /** If true, other values should be present. */
 export const success = true;
