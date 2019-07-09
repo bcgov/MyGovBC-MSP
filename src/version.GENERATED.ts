35bf21674c3f93f8442840fb993b212c39aff34e
Adam Coard <arcoard+github@gmail.com>
2019-07-09 12:28:14 -0400

    moh-common-lib@2.1.0 which is necessary for changes to AddressComponent
    Re-arranges entire AddressComponent, also adds input `disableGeocoder`

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'fe9b6e2e';
-export const buildTime = '7/4/2019 at 3:19:19 PM';
+export const gitCommit = '56ec21e0';
+export const buildTime = '7/9/2019 at 9:25:25 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '1.1.0';
 /** Human readable message  */
-export const message = 'Canonical Version: fe9b6e2e - 7/4/2019 at 3:19:19 PM (v1.1.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/fe9b6e2e';
+export const message = 'Canonical Version: 56ec21e0 - 7/9/2019 at 9:25:25 AM (v1.1.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/56ec21e0';
 /** If true, other values should be present. */
 export const success = true;
