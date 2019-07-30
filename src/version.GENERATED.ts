c87178a9872d1085820ab4c785880a71b3364165
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-07-30 15:07:55 -0400

    Removal of i18n folder

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '69f1a8dc';
-export const buildTime = '7/27/2019 at 9:41:41 AM';
+export const gitCommit = 'bd332ae9';
+export const buildTime = '7/30/2019 at 12:03:56 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 69f1a8dc - 7/27/2019 at 9:41:41 AM (v2.0.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/69f1a8dc';
+export const message = 'Canonical Version: bd332ae9 - 7/30/2019 at 12:03:56 PM (v2.0.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/bd332ae9';
 /** If true, other values should be present. */
 export const success = true;
