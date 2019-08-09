1efb5064581cbcc782fe3084440aed598915eed7
Abhimanyu raut <39009744+AbhimanyuRaut@users.noreply.github.com>
2019-08-09 11:37:35 -0700

    Update index.js

--- src/app/modules/benefit/pages/authorize-submit/i18n/data/en/index.js
+++ src/app/modules/benefit/pages/authorize-submit/i18n/data/en/index.js
@@ -6,7 +6,7 @@ module.exports = {
   agreeLabel: 'Yes, I agree',
   attorneyDoYouAgreeLabel: 'Do you have Power of Attorney to apply on behalf of {applicantName}?',
   continueButton: 'Submit Application',
-  declaration: "I hereby consent to the release of information from my income tax returns, and other taxpayer information, by the Canada Revenue Agency to the Ministry of Health and/or Health Insurance BC. The information obtained will be relevant to and used solely for the purpose of determining and verifying my initial and ongoing entitlement to the supplementary benefits program under the Medicare Protection Act, and/or other income-based programs, including Supplementary Benefits, Emergency Health Services and Healthy Kids. \
+  declaration: "I hereby consent to the release of information from my income tax returns, and other taxpayer information, by the Canada Revenue Agency to the Ministry of Health and/or Health Insurance BC. The information obtained will be relevant to and used solely for the purpose of determining and verifying my initial and ongoing entitlement to the supplementary benefits program under the <i>Medicare Protection Act</i>, and/or other income-based programs, including Supplementary Benefits, Emergency Health Services and Healthy Kids. \
   \
   This authorization is valid for the taxation year prior to the signature of this application. It may be revoked by sending a written notice to Health Insurance BC."
 }
