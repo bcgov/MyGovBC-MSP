a802acb1f7565531979555fd601fefe98fc1fbc1
cyril bravo <cyril.bravo@vs-dev015.maximusbchealth.local>
2019-08-16 12:55:05 -0400

    Fixed merge conflict in version.GENERATED.ts

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,18 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-<<<<<<< HEAD
-export const gitCommit = 'ad63ba0';
-export const buildTime = '8/13/2019 at 2:10:28 PM';
-/** App version retrieved from package.json. */
-export const projectVersion = '2.2.0';
-/** Human readable message  */
-export const message = 'Canonical Version: ad63ba0 - 8/13/2019 at 2:10:28 PM (v2.2.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/ad63ba0';
-=======
 export const gitCommit = '97e757ea';
 export const buildTime = '8/14/2019 at 12:29:24 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.2.0';
 /** Human readable message  */
 export const message = 'Canonical Version: 97e757ea - 8/14/2019 at 12:29:24 PM (v2.2.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/97e757ea';
->>>>>>> 7e770217c532db016d7475db12461e8d27f06a2b
 /** If true, other values should be present. */
 export const success = true;
