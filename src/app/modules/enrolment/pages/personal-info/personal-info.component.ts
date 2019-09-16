ff623f7664425d28ade45fb0afcf9d86f390ee86
kreed0422 <38866709+kreed0422@users.noreply.github.com>
2019-09-16 11:30:49 -0400

    Checkpoint - enrolment child working for status and support documents (#139)

--- src/app/modules/enrolment/pages/personal-info/personal-info.component.ts
+++ src/app/modules/enrolment/pages/personal-info/personal-info.component.ts
@@ -1,47 +1,54 @@
-import {Component, Injectable, ViewChild, ViewChildren,
-  ChangeDetectorRef, QueryList} from '@angular/core';
-// import {MspDataService} from '../../service/msp-data.service';
+import {Component, Injectable, ViewChildren, QueryList, OnInit, AfterViewInit, OnDestroy} from '@angular/core';
 import { MspDataService } from '../../../../services/msp-data.service';
 import { Router } from '@angular/router';
-import {NgForm} from '@angular/forms';
 import {PersonalDetailsComponent} from '../../components/personal-details/personal-details.component';
-import {BaseComponent} from '../../../../models/base.component';
-import { ServicesCardDisclaimerModalComponent } from '../../../msp-core/components/services-card-disclaimer/services-card-disclaimer.component';
 import { ROUTES_ENROL } from '../../models/enrol-route-constants';
 import { PageStateService } from '../../../../services/page-state.service';
 import { MspApplication } from '../../models/application.model';
 import { MspPerson } from '../../../account/models/account.model';
 import { StatusInCanada } from '../../../msp-core/models/canadian-status.enum';
-import { Relationship } from '../../../msp-core/models/relationship.enum';
-import { statusReasonRules } from '../../../msp-core/components/canadian-status/canadian-status.component';
 import { PersonDocuments } from '../../../../components/msp/model/person-document.model';
 import { yesNoLabels } from '../../../msp-core/models/msp-constants';
 import { nameChangeSupportDocuments } from '../../../msp-core/components/support-documents/support-documents.component';
+import { AbstractForm } from 'moh-common-lib';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   templateUrl: './personal-info.component.html'
 })
 @Injectable()
-export class PersonalInfoComponent extends BaseComponent {
+export class PersonalInfoComponent extends AbstractForm implements OnInit, AfterViewInit, OnDestroy {
 
-  @ViewChild('formRef') form: NgForm;
   @ViewChildren(PersonalDetailsComponent) personalDetailsComponent: QueryList<
     PersonalDetailsComponent
   >;
 
   yesNoRadioLabels = yesNoLabels;
   nameChangeDocList = nameChangeSupportDocuments();
+  subscriptions: Subscription[];
 
-  constructor( private dataService: MspDataService,
-               private _router: Router,
-               private pageStateService: PageStateService,
-               cd: ChangeDetectorRef ) {
-    super(cd);
+  constructor( protected router: Router,
+               private dataService: MspDataService,
+               private pageStateService: PageStateService ) {
+    super(router);
   }
 
   ngOnInit() {
-    this.pageStateService.setPageIncomplete(this._router.url, this.dataService.mspApplication.pageStatus);
+    this.pageStateService.setPageIncomplete(this.router.url, this.dataService.mspApplication.pageStatus);
+  }
+
+  ngAfterViewInit() {
+
+    if (this.form) {
+      this.subscriptions = [
+        this.form.valueChanges.subscribe(() => { this.dataService.saveMspApplication(); })
+        ];
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach( itm => itm.unsubscribe() );
   }
 
   get applicant(): MspPerson {
@@ -50,23 +57,21 @@ export class PersonalInfoComponent extends BaseComponent {
 
   set applicant( applicant: MspPerson ) {
     this.dataService.mspApplication.applicant = applicant;
-    this.dataService.saveMspApplication();
   }
 
   get statusDocuments(): PersonDocuments {
-    return this.dataService.mspApplication.applicant.documents;
+    return this.applicant.documents;
   }
 
   set statusDocuments( document: PersonDocuments ) {
 
     if ( document.images.length === 0 ) {
       // no status documents remove any name documents
-      this.dataService.mspApplication.applicant.nameChangeDocs.documentType = null;
-      this.dataService.mspApplication.applicant.nameChangeDocs.images = [];
+      this.applicant.nameChangeDocs.documentType = null;
+      this.applicant.nameChangeDocs.images = [];
     }
 
-    this.dataService.mspApplication.applicant.documents = document;
-    this.dataService.saveMspApplication();
+    this.applicant.documents = document;
   }
 
   get hasStatus() {
@@ -75,32 +80,32 @@ export class PersonalInfoComponent extends BaseComponent {
            this.applicant.currentActivity !== undefined;
   }
 
-  get nameChangeDocuments(): PersonDocuments {
-    return this.dataService.mspApplication.applicant.nameChangeDocs;
-  }
-
-  set nameChangeDocuments( document: PersonDocuments ) {
-    this.dataService.mspApplication.applicant.nameChangeDocs = document;
-    this.dataService.saveMspApplication();
-  }
-
-  get hasNameChange() {
-    return this.dataService.mspApplication.applicant.hasNameChange;
-  }
-
-  set hasNameChange( value: boolean ) {
-    this.dataService.mspApplication.applicant.hasNameChange = value;
-  }
-
   get requestNameChangeInfo() {
-    return this.hasStatus && this.hasNameChange && this.statusDocuments.images.length;
+    return this.hasStatus && this.applicant.hasNameChange && this.statusDocuments.images.length;
+  }
+
+
+  canContinue(): boolean {
+    let valid = super.canContinue() && this.statusDocuments.images.length > 0;
+    if ( this.applicant.hasNameChange ) {
+      valid = valid && this.applicant.nameChangeDocs.images.length > 0;
+    }
+    return valid;
+  }
+
+  continue(): void {
+    if (!this.canContinue()) {
+      console.log('Please fill in all required fields on the form.');
+      this.markAllInputsTouched();
+    }else{
+      this.pageStateService.setPageComplete(this.router.url, this.dataService.mspApplication.pageStatus);
+      this.navigate(ROUTES_ENROL.SPOUSE_INFO.fullpath);
+    }
   }
 
 
 
-  get application(): MspApplication {
-    return this.dataService.mspApplication;
-  }
+
 
 
 
@@ -148,29 +153,5 @@ export class PersonalInfoComponent extends BaseComponent {
       ...this.dataService.mspApplication.children
     ];
     return target.filter(x => x).filter(x => x.ineligibleForMSP).length >= 1;
-  }
-
-  continue(): void {
-    // console.log('personal info form itself valid: %s', this.form.valid);
-    console.log(
-      'combinedValidationState on personal info: %s',
-      this.isAllValid()
-    );
-    if (!this.isAllValid()) {
-      console.log('Please fill in all required fields on the form.');
-    }else{
-      this.pageStateService.setPageComplete(this._router.url, this.dataService.mspApplication.pageStatus);
-      this._router.navigate([ROUTES_ENROL.PERSONAL_INFO.fullpath]);
-    }
-  }
-
-  canContinue(): boolean {
-
-    console.log( 'form: ', this.form.valid , this.isAllValid() );
-    const valid = !(!this.isStayinginBCAfterstudies() ||
-    this.checkAnyDependentsIneligible() || !this.isAllValid());
-
-    //console.log('canContinue(): ', valid, this.form );
-    return valid;
   }
 }
