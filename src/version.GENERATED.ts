51f25b9955d079a99c7376466182dce2dcb3db60
cyril bravo <cyril.bravo@vs-dev015.maximusbchealth.local>
2019-09-27 18:29:25 -0400

    Changes in version.GENERATED.ts

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'e3ed0617';
-export const buildTime = '9/26/2019 at 12:50:58 PM';
+export const gitCommit = '4f7d10f';
+export const buildTime = '9/27/2019 at 3:23:00 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.5.2';
 /** Human readable message  */
-export const message = 'Canonical Version: e3ed0617 - 9/26/2019 at 12:50:58 PM (v2.5.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/e3ed0617';
+export const message = 'Canonical Version: 4f7d10f - 9/27/2019 at 3:23:00 PM (v2.5.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/4f7d10f';
 /** If true, other values should be present. */
 export const success = true;
