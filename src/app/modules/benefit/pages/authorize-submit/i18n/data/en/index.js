c7caecd1d1d6d4f36e8ab80eb28764dadf2329d2
Adam Coard <arcoard+github@gmail.com>
2019-10-25 13:57:57 -0400

    Update language on SuppBenefit Authorize page

--- src/app/modules/benefit/pages/authorize-submit/i18n/data/en/index.js
+++ src/app/modules/benefit/pages/authorize-submit/i18n/data/en/index.js
@@ -4,7 +4,7 @@ module.exports = {
   declarationAndConsentTitle: 'Declaration and Consent',
   doYouAgreeLabel: '{name} (or legal representative), do you agree?',
   agreeLabel: 'Yes, I agree',
-  attorneyDoYouAgreeLabel: 'Do you have Power of Attorney or another legal representation agreement to apply on behalf of {applicantName}?',
+  attorneyDoYouAgreeLabel: 'Note: If you have Power of Attorney document (or another legal representation document) to apply on behalf of {applicantName} please upload it.',  
   continueButton: 'Submit',
   declaration: "I hereby consent to the release of information from my income tax returns, and other taxpayer information, by the Canada Revenue Agency to the Ministry of Health and/or Health Insurance BC. The information obtained will be relevant to and used for the purpose of determining and verifying my initial and ongoing entitlement to the Supplementary Benefits Program under the <i>Medicare Protection Act</i>, and will not be disclosed to any other party. This authorization is valid for the taxation year prior to the signature of this application, the year of the signature and for each subsequent consecutive taxation year for which supplementary benefits is requested. It may be revoked by sending a written notice to Health Insurance BC."
 }
