e2a981d590621413a5530bfa78d0cb8caf225cfe
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-04 12:14:14 -0500

    fix for child radio box -namechange

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5fd4a272';
-export const buildTime = '10/31/2019 at 9:56:26 AM';
+export const gitCommit = '81cfa414';
+export const buildTime = '11/1/2019 at 8:15:20 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.13.4';
 /** Human readable message  */
-export const message = 'Canonical Version: 5fd4a272 - 10/31/2019 at 9:56:26 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/5fd4a272';
+export const message = 'Canonical Version: 81cfa414 - 11/1/2019 at 8:15:20 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/81cfa414';
 /** If true, other values should be present. */
 export const success = true;
