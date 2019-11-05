e26fe255a929c0b30c8e95a99315b75b610c2ae4
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-05 12:20:19 -0500

    Fixed errorMessages input on date component to be errorMessage so it works with updated library.

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'e2a981d5';
-export const buildTime = '11/4/2019 at 2:40:20 PM';
+export const gitCommit = '3bc35c0b';
+export const buildTime = '11/5/2019 at 9:20:14 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.13.4';
 /** Human readable message  */
-export const message = 'Canonical Version: e2a981d5 - 11/4/2019 at 2:40:20 PM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/e2a981d5';
+export const message = 'Canonical Version: 3bc35c0b - 11/5/2019 at 9:20:14 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/3bc35c0b';
 /** If true, other values should be present. */
 export const success = true;
