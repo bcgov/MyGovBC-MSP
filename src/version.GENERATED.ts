302b2695777795e6b1e3ea800633a4e773ef2464
Adam Coard <arcoard+github@gmail.com>
2019-11-05 13:33:13 -0500

    Remove unused section in main index to diff between this and old one easily

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '3bc35c0b';
-export const buildTime = '11/5/2019 at 9:20:14 AM';
+export const gitCommit = '8b3c3547';
+export const buildTime = '11/5/2019 at 9:51:42 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.13.4';
 /** Human readable message  */
-export const message = 'Canonical Version: 3bc35c0b - 11/5/2019 at 9:20:14 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/3bc35c0b';
+export const message = 'Canonical Version: 8b3c3547 - 11/5/2019 at 9:51:42 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/8b3c3547';
 /** If true, other values should be present. */
 export const success = true;
