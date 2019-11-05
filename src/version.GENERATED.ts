ae3dd6998ce77b8f3658cd6845452e969f531694
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-05 11:37:58 -0500

    CR-18884 -verbage

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '81cfa414';
-export const buildTime = '11/1/2019 at 8:15:20 AM';
+export const gitCommit = 'e2a981d5';
+export const buildTime = '11/4/2019 at 2:40:20 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.13.4';
 /** Human readable message  */
-export const message = 'Canonical Version: 81cfa414 - 11/1/2019 at 8:15:20 AM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/81cfa414';
+export const message = 'Canonical Version: e2a981d5 - 11/4/2019 at 2:40:20 PM (v2.13.4) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/e2a981d5';
 /** If true, other values should be present. */
 export const success = true;
