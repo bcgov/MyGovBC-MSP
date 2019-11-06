998cdf60625521a01246c2932321c05ad2f6cbb2
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-06 15:40:28 -0500

    fix for dob in retro pa and supp benefit

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '204eb439';
-export const buildTime = '11/6/2019 at 9:50:24 AM';
+export const gitCommit = 'a413ed0e';
+export const buildTime = '11/6/2019 at 12:29:02 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.14.0';
+export const projectVersion = '2.14.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 204eb439 - 11/6/2019 at 9:50:24 AM (v2.14.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/204eb439';
+export const message = 'Canonical Version: a413ed0e - 11/6/2019 at 12:29:02 PM (v2.14.1) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/a413ed0e';
 /** If true, other values should be present. */
 export const success = true;
