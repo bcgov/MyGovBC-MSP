82d5001246cb647d8c7cc4993584bc49ec2cba5b
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-06 13:41:38 -0500

    Updated package - moh-common-lib updated

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '204eb439';
-export const buildTime = '11/6/2019 at 9:50:24 AM';
+export const gitCommit = 'bf80f7f0';
+export const buildTime = '11/6/2019 at 10:37:35 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.14.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 204eb439 - 11/6/2019 at 9:50:24 AM (v2.14.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/204eb439';
+export const message = 'Canonical Version: bf80f7f0 - 11/6/2019 at 10:37:35 AM (v2.14.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/bf80f7f0';
 /** If true, other values should be present. */
 export const success = true;
