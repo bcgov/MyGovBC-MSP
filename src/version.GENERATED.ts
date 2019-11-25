06963df75e3d4b34b6f8407f255a10772081de07
Adam Coard <arcoard+github@gmail.com>
2019-11-25 13:01:42 -0500

    Create local docker setup

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '2cd2c623';
-export const buildTime = '11/18/2019 at 7:34:29 PM';
+export const gitCommit = '8d3574d9';
+export const buildTime = '11/22/2019 at 11:59:58 AM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.16.0';
+export const projectVersion = '2.17.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 2cd2c623 - 11/18/2019 at 7:34:29 PM (v2.16.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/2cd2c623';
+export const message = 'Canonical Version: 8d3574d9 - 11/22/2019 at 11:59:58 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/8d3574d9';
 /** If true, other values should be present. */
 export const success = true;
