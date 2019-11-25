28008eb0d3176826776081edf4319d2881b9868f
Adam Coard <arcoard+github@gmail.com>
2019-11-25 13:35:44 -0500

    Working! Checkpoint

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '8d3574d9';
-export const buildTime = '11/22/2019 at 11:59:58 AM';
+export const gitCommit = '82516779';
+export const buildTime = '11/25/2019 at 10:29:10 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.17.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 8d3574d9 - 11/22/2019 at 11:59:58 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/8d3574d9';
+export const message = 'Canonical Version: 82516779 - 11/25/2019 at 10:29:10 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/82516779';
 /** If true, other values should be present. */
 export const success = true;
