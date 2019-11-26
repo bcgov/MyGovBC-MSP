b7fe109ee5c09e0f3fb535e3935a939b5f92b93b
Adam Coard <arcoard+github@gmail.com>
2019-11-26 12:10:12 -0500

    Updated DEAM interim build

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '82516779';
-export const buildTime = '11/25/2019 at 10:29:10 AM';
+export const gitCommit = '28008eb0';
+export const buildTime = '11/26/2019 at 8:25:54 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.17.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 82516779 - 11/25/2019 at 10:29:10 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/82516779';
+export const message = 'Canonical Version: 28008eb0 - 11/26/2019 at 8:25:54 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/28008eb0';
 /** If true, other values should be present. */
 export const success = true;
