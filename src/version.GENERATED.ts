95979220f9f2eb23e8aa5a2240bf95eaf8dc01be
Adam Coard <arcoard+github@gmail.com>
2019-11-27 11:18:12 -0500

    Updated build for local nginx files

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '28008eb0';
-export const buildTime = '11/26/2019 at 8:25:54 AM';
+export const gitCommit = '04a4c4bb';
+export const buildTime = '11/27/2019 at 8:11:50 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.17.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 28008eb0 - 11/26/2019 at 8:25:54 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/28008eb0';
+export const message = 'Canonical Version: 04a4c4bb - 11/27/2019 at 8:11:50 AM (v2.17.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/04a4c4bb';
 /** If true, other values should be present. */
 export const success = true;
