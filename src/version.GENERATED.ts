ef19cb1d2eca389c271de9a4734c6418ab5aae83
Adam Coard <arcoard+github@gmail.com>
2019-12-11 12:18:16 -0500

    Passing in string instead of label to common-checkbox

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '87a0e06d';
-export const buildTime = '12/5/2019 at 9:10:37 AM';
+export const gitCommit = '07d266f0';
+export const buildTime = '12/11/2019 at 9:11:37 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.19.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 87a0e06d - 12/5/2019 at 9:10:37 AM (v2.19.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/87a0e06d';
+export const message = 'Canonical Version: 07d266f0 - 12/11/2019 at 9:11:37 AM (v2.19.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/07d266f0';
 /** If true, other values should be present. */
 export const success = true;
