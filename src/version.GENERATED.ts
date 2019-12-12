26cb302f493e591f414a7a8c32c5a902e8013f77
Adam Coard <arcoard+github@gmail.com>
2019-12-12 14:07:44 -0500

    Update language from comms as per Kai
    Kai says that we should remove the date logic as it's not relevant, just say the 'you might not qualify' part

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '07d266f0';
-export const buildTime = '12/11/2019 at 9:11:37 AM';
+export const gitCommit = '3e5170bd';
+export const buildTime = '12/12/2019 at 10:50:15 AM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.19.2';
+export const projectVersion = '2.20.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 07d266f0 - 12/11/2019 at 9:11:37 AM (v2.19.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/07d266f0';
+export const message = 'Canonical Version: 3e5170bd - 12/12/2019 at 10:50:15 AM (v2.20.0) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/3e5170bd';
 /** If true, other values should be present. */
 export const success = true;
