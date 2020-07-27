36bc754a3eba96827a9735f14aa044eb420e986b
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-07-27 00:28:27 -0700

    DEAM-75: Update spec for account module
    Please review and approve when convenient
    
    What I did:
    Updated the spec for the account module. At some point `processService`
    was added to the constructor but the spec was not updated accordingly.So
    it was expecting an argument for processService but not receiving one.
    Tests should run as normal now

--- src/app/modules/account/account.module.spec.ts
+++ src/app/modules/account/account.module.spec.ts
@@ -1,10 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { ProcessService } from '../../services/process.service';
 import { AccountModule } from './account.module';
 
 describe('AccountModule', () => {
   let accountModule: AccountModule;
 
   beforeEach(() => {
-    accountModule = new AccountModule();
+    const processServiceStub = () => ({ init: array => ({}) });
+
+    TestBed.configureTestingModule({
+      providers: [
+        AccountModule,
+        { provide: ProcessService, useFactory: processServiceStub }
+      ]
+    });
+
+    accountModule = TestBed.get(AccountModule);
   });
 
   it('should create an instance', () => {
