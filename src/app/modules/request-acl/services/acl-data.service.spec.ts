1cd39beda4f8f85272f05a2775355057ce211196
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-07-31 14:33:29 -0700

    DEAM-400: Update unit tests in request-acl
    There was 4 tests failing out of 5, there should now be 11 passing

--- src/app/modules/request-acl/services/acl-data.service.spec.ts
+++ src/app/modules/request-acl/services/acl-data.service.spec.ts
@@ -1,12 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { LocalStorageService } from 'angular-2-local-storage';
 import { AclDataService } from './acl-data.service';
 
 describe('AclDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AclDataService;
+  beforeEach(() => {
+    const localStorageServiceStub = () => ({
+      set: (_storageKey, dto) => ({}),
+      get: _storageKey => ({})
+    });
 
-  it('should be created', () => {
-    const service: AclDataService = TestBed.get(AclDataService);
+    TestBed.configureTestingModule({
+      providers: [
+        AclDataService,
+        { provide: LocalStorageService, useFactory: localStorageServiceStub }
+      ]
+    });
+
+    service = TestBed.get(AclDataService);
+  });
+
+  it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('saveApplication', () => {
+    it('makes expected call', () => {
+      const localStorageServiceStub: LocalStorageService = TestBed.get(
+        LocalStorageService
+      );
+      spyOn(localStorageServiceStub, 'set').and.callThrough();
+      service.saveApplication();
+      expect(localStorageServiceStub.set).toHaveBeenCalled();
+    });
   });
 });
