423c0c125b6e4ac6346c0c5ae48b4672da35b74e
Harry Maynard <harry.maynard@maximusbc.ca>
2020-08-13 17:29:05 -0400

    DEAM-407: Added unit tests.

--- src/app/modules/account/components/moving-information/moving-information.component.spec.ts
+++ src/app/modules/account/components/moving-information/moving-information.component.spec.ts
@@ -5,7 +5,7 @@ import { Relationship } from 'app/models/relationship.enum';
 import { SharedCoreModule } from 'moh-common-lib';
 import { FormsModule } from '@angular/forms';
 import { MspPerson } from '../../../../components/msp/model/msp-person.model';
-import { CanadianStatusReason } from '../../../msp-core/models/canadian-status.enum';
+import { CanadianStatusReason, StatusInCanada } from '../../../msp-core/models/canadian-status.enum';
 
 fdescribe('ChildMovingInformationComponent', () => {
   let component: ChildMovingInformationComponent;
@@ -94,16 +94,272 @@ fdescribe('ChildMovingInformationComponent', () => {
   });
 
   describe('isLivedInBCSinceBirth', () => {
-    it('should return the true if lived in BC since birth', () => {
+    it('should return true if lived in BC since birth', () => {
       component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
       expect(component.isLivedInBCSinceBirth).toBe(true);
     });
 
-    it('should return the false if haven\'t lived in BC since birth', () => {
+    it('should return false if haven\'t lived in BC since birth', () => {
       component.person.currentActivity = undefined;
       expect(component.isLivedInBCSinceBirth).toBe(false);
       component.person.currentActivity = CanadianStatusReason.LivingInBCWithoutMSP;
       expect(component.isLivedInBCSinceBirth).toBe(false);
     });
   });
+
+  describe('isCanadianFromProv', () => {
+    it('should return true if is Canadian citizen and moving from province.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.isCanadianFromProv).toBe(true);
+    });
+
+    it('should return false if not Canadian citizen or not moving from province.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.isCanadianFromProv).toBe(false);
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.isCanadianFromProv).toBe(false);
+    });
+  });
+
+  describe('isCanadianFromCountry', () => {
+    it('should return true if is Canadian citizen and moving from country.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.isCanadianFromCountry).toBe(true);
+    });
+
+    it('should return false if not Canadian citizen or not moving from country.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.isCanadianFromCountry).toBe(false);
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.Visiting;
+      expect(component.isCanadianFromCountry).toBe(false);
+    });
+  });
+
+  describe('isCanadianNotBC', () => {
+    it('should return true if is Canadian citizen.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      expect(component.isCanadianNotBC).toBe(true);
+    });
+
+    it('should return false if not Canadian citizen.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      expect(component.isCanadianNotBC).toBe(false);
+    });
+  });
+
+  describe('isResidentFromProv', () => {
+    it('should return true if is Permanent Resident and moving from province.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.isResidentFromProv).toBe(true);
+    });
+
+    it('should return false if not Permanent Resident or not moving from province.', () => {
+      component.person.status = StatusInCanada.TemporaryResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.isResidentFromProv).toBe(false);
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.Visiting;
+      expect(component.isResidentFromProv).toBe(false);
+    });
+  });
+
+  describe('isResidentFromCountry', () => {
+    it('should return true if is Permanent Resident and moving from province.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.isResidentFromCountry).toBe(true);
+    });
+
+    it('should return false if not Permanent Resident or not moving from province.', () => {
+      component.person.status = StatusInCanada.TemporaryResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.isResidentFromCountry).toBe(false);
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.Visiting;
+      expect(component.isResidentFromCountry).toBe(false);
+    });
+  });
+
+  describe('isResidentNotBC', () => {
+    it('should return true if is Permanent Resident and Living In BC Without MSP.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.LivingInBCWithoutMSP;
+      expect(component.isResidentNotBC).toBe(true);
+    });
+
+    it('should return false if not Permanent Resident or not Living In BC Without MSP.', () => {
+      component.person.status = StatusInCanada.TemporaryResident;
+      component.person.currentActivity = CanadianStatusReason.LivingInBCWithoutMSP;
+      expect(component.isResidentNotBC).toBe(false);
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.Visiting;
+      expect(component.isResidentNotBC).toBe(false);
+    });
+  });
+
+  describe('isTemporaryResident', () => {
+    it('should return true if is Temporary Resident.', () => {
+      component.person.status = StatusInCanada.TemporaryResident;
+      expect(component.isTemporaryResident).toBe(true);
+    });
+
+    it('should return false if not Temporary Resident.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      expect(component.isTemporaryResident).toBe(false);
+    });
+  });
+
+  describe('requestSchoolInfo', () => {
+    it('should return true if should request school info.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      expect(component.requestSchoolInfo).toBe(true);
+    });
+
+    it('should return false if should not request school info.', () => {
+      expect(component.requestSchoolInfo).toBe(false);
+    });
+  });
+
+  describe('requestPermMoveInfo', () => {
+    it('should return true if should request permanent move info.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.livedInBCSinceBirth = true;
+      expect(component.requestPermMoveInfo).toBe(true);
+    });
+
+    it('should return false if should not request permanent move info.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.livedInBCSinceBirth = false;
+      expect(component.requestPermMoveInfo).toBe(false);
+    });
+  });
+
+  describe('arrivalDateRequired', () => {
+    it('should return true if arrival date is required.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.arrivalDateRequired).toBe(true);
+    });
+
+    it('should return false if arrival date is not required.', () => {
+      expect(component.arrivalDateRequired).toBe(false);
+    });
+  });
+
+  describe('requestAdditionalMoveInfo', () => {
+    it('should return true if need to request additional move info.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.livedInBCSinceBirth = true;
+      expect(component.requestAdditionalMoveInfo).toBe(true);
+    });
+
+    it('should return false if don\'t need to request additional move info.', () => {
+      expect(component.requestAdditionalMoveInfo).toBe(false);
+    });
+  });
+
+  describe('possessiveRelationshipNoun', () => {
+    it('should return "you" if relationship is Applicant.', () => {
+      component.person.relationship = Relationship.Applicant;
+      expect(component.possessiveRelationshipNoun).toBe('you');
+    });
+
+    it('should return "your spouse" if relationship is Spouse.', () => {
+      component.person.relationship = Relationship.Spouse;
+      expect(component.possessiveRelationshipNoun).toBe('your spouse');
+    });
+
+    it('should return "this child" for all other cases.', () => {
+      component.person.relationship = Relationship.ChildUnder19;
+      expect(component.possessiveRelationshipNoun).toBe('this child');
+    });
+  });
+
+  describe('showlivedInBC', () => {
+    it('should return true if status is Canadian citizen.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      expect(component.showlivedInBC).toBe(true);
+    });
+
+    it('should return false if status isn\'t Canadian citizen.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      expect(component.showlivedInBC).toBe(false);
+    });
+  });
+
+  describe('showRecentMove', () => {
+    it('should return true if should show recent move.', () => {
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      component.person.livedInBCSinceBirth = false;
+      expect(component.showRecentMove).toBe(true);
+    });
+
+    it('should return true if should show recent move.', () => {
+      component.person.relationship = Relationship.ChildUnder19;
+      component.person.livedInBCSinceBirth = false;
+      expect(component.showRecentMove).toBe(true);
+    });
+
+    it('should return true if should not show recent move.', () => {
+      expect(component.showRecentMove).toBe(false);
+    });
+  });
+
+  describe('showFromProv', () => {
+    it('should return false if is child.', () => {
+      component.person.relationship = Relationship.ChildUnder19;
+      expect(component.showFromProv).toBe(false);
+    });
+
+    it('should return true if Canadian citizen and moving from province.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.showFromProv).toBe(true);
+    });
+
+    it('should return true if Canadian citizen and living in BC without MSP.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.LivingInBCWithoutMSP;
+      expect(component.showFromProv).toBe(true);
+    });
+
+    it('should return true if permanent resident and moving from province.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromProvince;
+      expect(component.showFromProv).toBe(true);
+    });
+  });
+
+  fdescribe('showFromCountry', () => {
+    it('should return false if is child.', () => {
+      component.person.relationship = Relationship.ChildUnder19;
+      expect(component.showFromCountry).toBe(false);
+    });
+
+    it('should return true if Canadian citizen and moving from country.', () => {
+      component.person.status = StatusInCanada.CitizenAdult;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.showFromCountry).toBe(true);
+    });
+
+    it('should return true if permanent resident and moving from country.', () => {
+      component.person.status = StatusInCanada.PermanentResident;
+      component.person.currentActivity = CanadianStatusReason.MovingFromCountry;
+      expect(component.showFromCountry).toBe(true);
+    });
+
+    it('should return true if temporary resident.', () => {
+      component.person.status = StatusInCanada.TemporaryResident;
+      expect(component.showFromCountry).toBe(true);
+    });
+  });
+
+  
 });
